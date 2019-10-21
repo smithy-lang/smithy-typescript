d86ceb656d3c1474f903c3260d156d4f36d9bfc8
Michael Dowling <mtdowling@gmail.com>
2019-10-21 16:04:50 -0700

    Add test for protocol name sanitizer

--- smithy-typescript-codegen/src/test/java/software/amazon/smithy/typescript/codegen/integration/ProtocolGeneratorTest.java
+++ smithy-typescript-codegen/src/test/java/software/amazon/smithy/typescript/codegen/integration/ProtocolGeneratorTest.java
@@ -1,11 +1,14 @@
 package software.amazon.smithy.typescript.codegen.integration;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 import org.junit.jupiter.api.Test;
 
 public class ProtocolGeneratorTest {
     @Test
-    public void providesBuiltinNameSanitation() {
-        // TODO
+    public void sanitizesNames() {
+        assertThat(ProtocolGenerator.getSanitizedName("aws.rest-json.1.1"), equalTo("Aws_restJson_1_1"));
     }
 
     @Test
