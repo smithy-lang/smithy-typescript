49cc7bf126daa23aba20bd89b733a6eb3395b684
AllanFly120 <zheallan@amazon.com>
2019-10-28 17:39:08 -0700

    feat: standardize plugins (#422)

---
+++ packages/middleware-retry/src/retryDecider.ts
@@ -0,0 +1,35 @@
+import { RETRYABLE_STATUS_CODES } from "./constants";
+import {
+  isClockSkewError,
+  isStillProcessingError,
+  isThrottlingError
+} from "@aws-sdk/service-error-classification";
+import { MetadataBearer, SdkError } from "@aws-sdk/types";
+
+export const defaultRetryDecider = (error: SdkError) => {
+  if (!error) {
+    return false;
+  }
+
+  if (error.connectionError) {
+    return true;
+  }
+
+  if (
+    hasMetadata(error) &&
+    error.$metadata.httpStatusCode &&
+    RETRYABLE_STATUS_CODES.has(error.$metadata.httpStatusCode)
+  ) {
+    return true;
+  }
+
+  return (
+    isStillProcessingError(error) ||
+    isThrottlingError(error) ||
+    isClockSkewError(error)
+  );
+};
+
+function hasMetadata(error: any): error is MetadataBearer {
+  return error && error.$metadata;
+}
