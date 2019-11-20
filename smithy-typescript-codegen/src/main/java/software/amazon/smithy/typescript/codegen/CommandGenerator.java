2344f57fa1d18e1cbaf72c0eb3ac6fb955f642e4
Chase Coalwell <chasecoa@amazon.com>
2019-11-20 12:12:36 -0800

    remove optional generic type args

--- smithy-typescript-codegen/src/main/java/software/amazon/smithy/typescript/codegen/CommandGenerator.java
+++ smithy-typescript-codegen/src/main/java/software/amazon/smithy/typescript/codegen/CommandGenerator.java
@@ -47,8 +47,6 @@ final class CommandGenerator implements Runnable {
     private final OperationIndex operationIndex;
     private final String inputType;
     private final String outputType;
-    private final String serviceInputTypes;
-    private final String serviceOutputTypes;
     private final ApplicationProtocol applicationProtocol;
 
     CommandGenerator(
@@ -75,8 +73,6 @@ final class CommandGenerator implements Runnable {
         operationIndex = model.getKnowledge(OperationIndex.class);
         inputType = symbol.getName() + "Input";
         outputType = symbol.getName() + "Output";
-        serviceInputTypes = "ServiceInputTypes";
-        serviceOutputTypes = "ServiceOutputTypes";
     }
 
     @Override
@@ -86,8 +82,8 @@ final class CommandGenerator implements Runnable {
 
         // Add required imports.
         writer.addImport(configType, configType, serviceSymbol.getNamespace());
-        writer.addImport(serviceInputTypes, serviceInputTypes, serviceSymbol.getNamespace());
-        writer.addImport(serviceOutputTypes, serviceOutputTypes, serviceSymbol.getNamespace());
+        writer.addImport("ServiceInputTypes", "ServiceInputTypes", serviceSymbol.getNamespace());
+        writer.addImport("ServiceOutputTypes", "ServiceOutputTypes", serviceSymbol.getNamespace());
         writer.addImport("Command", "$Command", "@aws-sdk/smithy-client");
         writer.addImport("FinalizeHandlerArguments", "FinalizeHandlerArguments", "@aws-sdk/types");
         writer.addImport("Handler", "Handler", "@aws-sdk/types");
@@ -98,8 +94,14 @@ final class CommandGenerator implements Runnable {
         addInputAndOutputTypes();
 
         String name = symbol.getName();
-        Object[] params = {name, serviceInputTypes, inputType, serviceOutputTypes, outputType, configType};
-        writer.openBlock("export class $L extends $$Command<$L, $L, $L, $L, $L> {", "}", params, () -> {
+        writer.openBlock(
+                "export class $L extends $$Command<$L, $L, $L> {",
+                "}",
+                name,
+                inputType,
+                outputType,
+                configType,
+                () -> {
 
             // Section for adding custom command properties.
             writer.write("// Start section: $L", COMMAND_PROPERTIES_SECTION);
