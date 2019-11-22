496d9762887bd7b0c78b861cd1145acb122575c8
Chase Coalwell <chasecoa@amazon.com>
2019-11-22 12:47:02 -0800

    add IndexGeneratorTest

---
+++ smithy-typescript-codegen/src/test/java/software/amazon/smithy/typescript/codegen/IndexGeneratorTest.java
@@ -0,0 +1,31 @@
+package software.amazon.smithy.typescript.codegen;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+
+import org.junit.jupiter.api.Test;
+import software.amazon.smithy.build.MockManifest;
+import software.amazon.smithy.codegen.core.SymbolProvider;
+import software.amazon.smithy.model.Model;
+import software.amazon.smithy.model.node.Node;
+
+public class IndexGeneratorTest {
+
+    @Test
+    public void writesIndex() {
+        Model model = Model.assembler().addImport(getClass().getResource("testmodel.smithy")).assemble().unwrap();
+        TypeScriptSettings settings = TypeScriptSettings.from(model, Node.objectNodeBuilder()
+                .withMember("service", Node.from("smithy.example#Example"))
+                .withMember("package", Node.from("example"))
+                .withMember("packageVersion", Node.from("1.0.0"))
+                .build());
+        SymbolProvider symbolProvider = TypeScriptCodegenPlugin.createSymbolProvider(model);
+        MockManifest manifest = new MockManifest();
+
+        IndexGenerator.writeIndex(settings, model, symbolProvider, manifest);
+
+        String contents = manifest.getFileString("index.ts").get();
+        
+        assertThat(contents, containsString("export"));
+    }
+}
