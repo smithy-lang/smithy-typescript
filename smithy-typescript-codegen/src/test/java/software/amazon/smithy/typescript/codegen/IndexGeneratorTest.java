6cbfec0c89dfe74683c4dec2f4200d9207c4972c
Chase Coalwell <chasecoa@amazon.com>
2019-11-25 09:01:20 -0800

    test commands are exported

--- smithy-typescript-codegen/src/test/java/software/amazon/smithy/typescript/codegen/IndexGeneratorTest.java
+++ smithy-typescript-codegen/src/test/java/software/amazon/smithy/typescript/codegen/IndexGeneratorTest.java
@@ -1,6 +1,7 @@
 package software.amazon.smithy.typescript.codegen;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.containsString;
 
 import org.junit.jupiter.api.Test;
@@ -13,7 +14,7 @@ public class IndexGeneratorTest {
 
     @Test
     public void writesIndex() {
-        Model model = Model.assembler().addImport(getClass().getResource("simple-service.smithy")).assemble().unwrap();
+        Model model = Model.assembler().addImport(getClass().getResource("simple-service-with-operation.smithy")).assemble().unwrap();
         TypeScriptSettings settings = TypeScriptSettings.from(model, Node.objectNodeBuilder()
                 .withMember("service", Node.from("smithy.example#Example"))
                 .withMember("package", Node.from("example"))
@@ -26,6 +27,6 @@ public class IndexGeneratorTest {
 
         String contents = manifest.getFileString("index.ts").get();
         assertThat(contents, containsString("export * from \"./Example\";"));
-        // TODO test index contains command exports
+        assertThat(contents, containsString("export * from \"./commands/GetFooCommand\";"));
     }
 }
