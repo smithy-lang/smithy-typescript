623f9df8cf0c9b2c866f26170443c0fbc782f59e
Allan Zheng <zheallan@amazon.com>
2019-12-05 00:23:39 -0800

    move host header mw to aws codegen and add event stream dependencies

--- smithy-typescript-codegen/src/main/java/software/amazon/smithy/typescript/codegen/TypeScriptDependency.java
+++ smithy-typescript-codegen/src/main/java/software/amazon/smithy/typescript/codegen/TypeScriptDependency.java
@@ -39,7 +39,6 @@ public enum TypeScriptDependency implements SymbolDependencyContainer {
     MIDDLEWARE_USER_AGENT("dependencies", "@aws-sdk/middleware-user-agent", "^0.1.0-preview.1", true),
     MIDDLEWARE_RETRY("dependencies", "@aws-sdk/middleware-retry", "^0.1.0-preview.5", true),
     MIDDLEWARE_STACK("dependencies", "@aws-sdk/middleware-stack", "^0.1.0-preview.6", true),
-    MIDDLEWARE_HOST_HEADER("dependencies", "@aws-sdk/middleware-host-header", "^0.1.0-preview.1", true),
 
     AWS_CRYPTO_SHA256_BROWSER("dependencies", "@aws-crypto/sha256-browser", "^0.1.0-preview.1", true),
     AWS_SDK_HASH_NODE("dependencies", "@aws-sdk/hash-node", "^0.1.0-preview.5", true),
@@ -69,7 +68,12 @@ public enum TypeScriptDependency implements SymbolDependencyContainer {
 
     // Conditionally added if a big decimal shape is found in a model.
     BIG_JS("dependencies", "big.js", "^5.2.2", false),
-    TYPES_BIG_JS("devDependencies", "@types/big.js", "^4.0.5", false);
+    TYPES_BIG_JS("devDependencies", "@types/big.js", "^4.0.5", false),
+
+    // Conditionally added if a event stream shape is found anywhere in the model
+    AWS_SDK_UTIL_EVENT_STREAM_NODE("dependencies", "@aws-sdk/util-eventstream-node", "^0.1.0-preview.6", false),
+    // Conditionally added if a event stream shape found in model's input
+    MIDDLEWARE_EVENT_STREAM("dependencies", "@aws-sdk/middleware-event-stream", "^0.1.0-preview.6", false);
 
     public static final String NORMAL_DEPENDENCY = "dependencies";
     public static final String DEV_DEPENDENCY = "devDependencies";
