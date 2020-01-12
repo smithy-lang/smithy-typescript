d7a57b296ee0e73113412bcad97d1d415a94ed3b
Allan Zheng <zheallan@amazon.com>
2020-01-12 11:49:04 -0800

    add error name to error deser

--- smithy-typescript-codegen/src/main/java/software/amazon/smithy/typescript/codegen/integration/HttpBindingProtocolGenerator.java
+++ smithy-typescript-codegen/src/main/java/software/amazon/smithy/typescript/codegen/integration/HttpBindingProtocolGenerator.java
@@ -625,6 +625,7 @@ public abstract class HttpBindingProtocolGenerator implements ProtocolGenerator 
                        + "): Promise<$T> => {", "};",
                 errorDeserMethodName, isErrorCodeInBody ? "parsedOutput" : "output", errorSymbol, () -> {
             writer.openBlock("const contents: $T = {", "};", errorSymbol, () -> {
+                writer.write("name: $S,", error.getId().getName());
                 writer.write("__type: $S,", error.getId().getName());
                 writer.write("$$fault: $S,", error.getTrait(ErrorTrait.class).get().getValue());
                 writer.write("$$metadata: deserializeMetadata(output),");
