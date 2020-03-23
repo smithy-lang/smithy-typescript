dc7b53f681b5e7d28a8559ac068c3d134ed180d8
stickevi <stickevi@amazon.com>
2020-03-23 08:45:11 -0700

    Add default jest config to packages

--- smithy-typescript-codegen/src/main/java/software/amazon/smithy/typescript/codegen/CodegenVisitor.java
+++ smithy-typescript-codegen/src/main/java/software/amazon/smithy/typescript/codegen/CodegenVisitor.java
@@ -58,6 +58,7 @@ class CodegenVisitor extends ShapeVisitor.Default<Void> {
 
     /** A mapping of static resource files to copy over to a new filename. */
     private static final Map<String, String> STATIC_FILE_COPIES = MapUtils.of(
+            "jest.config.js", "jest.config.js",
             "tsconfig.es.json", "tsconfig.es.json",
             "tsconfig.json", "tsconfig.json"
     );
