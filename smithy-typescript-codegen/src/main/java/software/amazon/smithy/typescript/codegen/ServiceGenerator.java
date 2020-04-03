961966110c886a5600a94ab1444f7c6fbb4940dd
Allan Zheng <zheallan@amazon.com>
2020-04-03 00:57:09 -0700

    client destroy method will call SmithyClient destroy by default

--- smithy-typescript-codegen/src/main/java/software/amazon/smithy/typescript/codegen/ServiceGenerator.java
+++ smithy-typescript-codegen/src/main/java/software/amazon/smithy/typescript/codegen/ServiceGenerator.java
@@ -324,6 +324,8 @@ final class ServiceGenerator implements Runnable {
         // Generates the destroy() method, and calls the destroy() method of
         // any runtime plugin that claims to have a destroy method.
         writer.openBlock("destroy(): void {", "}", () -> {
+            // Always call destroy() in SmithyClient class. By default, it's optional.
+            writer.write("super.destroy();");
             writer.pushState(CLIENT_DESTROY_SECTION);
             for (RuntimeClientPlugin plugin : runtimePlugins) {
                 plugin.getDestroyFunction().ifPresent(destroy -> {
