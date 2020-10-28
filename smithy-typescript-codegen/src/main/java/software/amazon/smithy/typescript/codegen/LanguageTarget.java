6dda98d93e1904ec27be9ab2ebb28690c0677e2e
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-10-28 03:50:49 +0000

    chore: move runtimeConfig to src folder

--- smithy-typescript-codegen/src/main/java/software/amazon/smithy/typescript/codegen/LanguageTarget.java
+++ smithy-typescript-codegen/src/main/java/software/amazon/smithy/typescript/codegen/LanguageTarget.java
@@ -65,6 +65,6 @@ public enum LanguageTarget {
     abstract String getTemplateFileName();
 
     String getTargetFilename() {
-        return getTemplateFileName().replace(".template", "");
+        return "./src/" + getTemplateFileName().replace(".template", "");
     }
 }
