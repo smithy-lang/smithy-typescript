bdbc60c66e37adb95af419362a57daaa41b0d3d6
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-09-28 03:38:24 +0000

    fix: modular client import in aggregated client

--- smithy-typescript-codegen/src/main/java/software/amazon/smithy/typescript/codegen/NonModularServiceGenerator.java
+++ smithy-typescript-codegen/src/main/java/software/amazon/smithy/typescript/codegen/NonModularServiceGenerator.java
@@ -70,7 +70,10 @@ final class NonModularServiceGenerator implements Runnable {
 
         // Generate the client and extend from the modular client.
         writer.writeShapeDocs(service);
-        writer.openBlock("export class $L extends $T {", "}", nonModularName, serviceSymbol, () -> {
+        String serviceSymbolName = serviceSymbol.getName();
+        writer.addImport(serviceSymbolName, serviceSymbolName,
+                "./" + CodegenUtils.SOURCE_FOLDER + serviceSymbol.getNamespace().substring(1));
+        writer.openBlock("export class $L extends $L {", "}", nonModularName, serviceSymbolName, () -> {
             Set<OperationShape> containedOperations = new TreeSet<>(topDownIndex.getContainedOperations(service));
             for (OperationShape operation : containedOperations) {
                 Symbol operationSymbol = symbolProvider.toSymbol(operation);
