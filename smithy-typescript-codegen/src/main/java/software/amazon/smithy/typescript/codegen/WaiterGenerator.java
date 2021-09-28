fa8c1e222b27776c49129a61aeac45ab818ea467
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-09-28 04:14:23 +0000

    fix: Client import in waiters

--- smithy-typescript-codegen/src/main/java/software/amazon/smithy/typescript/codegen/WaiterGenerator.java
+++ smithy-typescript-codegen/src/main/java/software/amazon/smithy/typescript/codegen/WaiterGenerator.java
@@ -75,12 +75,16 @@ class WaiterGenerator implements Runnable {
         writer.addImport("checkExceptions", "checkExceptions", WAITABLE_UTIL_PACKAGE);
         writer.addImport("WaiterConfiguration", "WaiterConfiguration", WAITABLE_UTIL_PACKAGE);
 
+        String serviceSymbolName = serviceSymbol.getName();
+        writer.addImport(serviceSymbolName, serviceSymbolName,
+                "./" + CodegenUtils.SOURCE_FOLDER + serviceSymbol.getNamespace().substring(1));
+
         // generates (deprecated) WaitFor....
         writer.writeDocs(waiter.getDocumentation().orElse("") + " \n"
                 + " @deprecated Use waitUntil" + waiterName + " instead. "
                 + "waitFor" + waiterName + " does not throw error in non-success cases.");
-        writer.openBlock("export const waitFor$L = async (params: WaiterConfiguration<$T>, input: $T): "
-                + "Promise<WaiterResult> => {", "}", waiterName, serviceSymbol, inputSymbol, () -> {
+        writer.openBlock("export const waitFor$L = async (params: WaiterConfiguration<$L>, input: $T): "
+                + "Promise<WaiterResult> => {", "}", waiterName, serviceSymbolName, inputSymbol, () -> {
             writer.write("const serviceDefaults = { minDelay: $L, maxDelay: $L };", waiter.getMinDelay(),
                             waiter.getMaxDelay());
             writer.write("return createWaiter({...serviceDefaults, ...params}, input, checkState);");
@@ -90,8 +94,8 @@ class WaiterGenerator implements Runnable {
         writer.writeDocs(waiter.getDocumentation().orElse("") + " \n"
                 + " @param params - Waiter configuration options.\n"
                 + " @param input - The input to " + operationSymbol.getName() + " for polling.");
-        writer.openBlock("export const waitUntil$L = async (params: WaiterConfiguration<$T>, input: $T): "
-                + "Promise<WaiterResult> => {", "}", waiterName, serviceSymbol, inputSymbol, () -> {
+        writer.openBlock("export const waitUntil$L = async (params: WaiterConfiguration<$L>, input: $T): "
+                + "Promise<WaiterResult> => {", "}", waiterName, serviceSymbolName, inputSymbol, () -> {
             writer.write("const serviceDefaults = { minDelay: $L, maxDelay: $L };", waiter.getMinDelay(),
                     waiter.getMaxDelay());
             writer.write("const result = await createWaiter({...serviceDefaults, ...params}, input, checkState);");
@@ -100,8 +104,8 @@ class WaiterGenerator implements Runnable {
     }
 
     private void generateAcceptors() {
-        writer.openBlock("const checkState = async (client: $T, input: $T): Promise<WaiterResult> => {", "}",
-                serviceSymbol, inputSymbol, () -> {
+        writer.openBlock("const checkState = async (client: $L, input: $T): Promise<WaiterResult> => {", "}",
+                serviceSymbol.getName(), inputSymbol, () -> {
                     writer.write("let reason;");
                     writer.openBlock("try {", "}", () -> {
                         writer.write("let result: any = await client.send(new $T(input))", operationSymbol);
