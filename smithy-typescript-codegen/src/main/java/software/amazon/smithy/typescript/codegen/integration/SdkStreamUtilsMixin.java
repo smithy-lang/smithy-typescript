cc87f2dd734a1e95bc705c59d8b855626e8c0c19
AllanZhengYP <zheallan@amazon.com>
2022-07-09 01:21:32 +0000

    generate util functions to consume response stream

---
+++ smithy-typescript-codegen/src/main/java/software/amazon/smithy/typescript/codegen/integration/SdkStreamUtilsMixin.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package software.amazon.smithy.typescript.codegen.integration;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Consumer;
+import software.amazon.smithy.codegen.core.SymbolProvider;
+import software.amazon.smithy.model.Model;
+import software.amazon.smithy.model.knowledge.TopDownIndex;
+import software.amazon.smithy.model.shapes.BlobShape;
+import software.amazon.smithy.model.shapes.MemberShape;
+import software.amazon.smithy.model.shapes.OperationShape;
+import software.amazon.smithy.model.shapes.ServiceShape;
+import software.amazon.smithy.model.shapes.Shape;
+import software.amazon.smithy.model.shapes.StructureShape;
+import software.amazon.smithy.model.traits.StreamingTrait;
+import software.amazon.smithy.typescript.codegen.LanguageTarget;
+import software.amazon.smithy.typescript.codegen.TypeScriptDependency;
+import software.amazon.smithy.typescript.codegen.TypeScriptSettings;
+import software.amazon.smithy.typescript.codegen.TypeScriptWriter;
+import software.amazon.smithy.utils.MapUtils;
+import software.amazon.smithy.utils.SmithyInternalApi;
+
+/**
+ * Add runtime config for injecting utility functions to consume the JavaScript
+ * runtime-specific stream implementations.
+ */
+@SmithyInternalApi
+public final class SdkStreamUtilsMixin implements TypeScriptIntegration {
+
+    @Override
+    public void addConfigInterfaceFields(
+        TypeScriptSettings settings,
+        Model model,
+        SymbolProvider symbolProvider,
+        TypeScriptWriter writer
+    ) {
+        if (!hasStreamingBlobResponse(model, settings.getService(model))) {
+            return;
+        }
+
+        writer.addImport("SdkStreamMixinInjector", "__SdkStreamMixinInjector",
+                TypeScriptDependency.AWS_SDK_TYPES.packageName);
+        writer.writeDocs("The internal function that inject utilities to runtime-specific stream to help users"
+                + " consume the data");
+        writer.write("sdkStreamMixin?: __SdkStreamMixinInjector;\n");
+    }
+
+    @Override
+    public Map<String, Consumer<TypeScriptWriter>> getRuntimeConfigWriters(
+        TypeScriptSettings settings,
+        Model model,
+        SymbolProvider symbolProvider,
+        LanguageTarget target
+    ) {
+        if (!hasStreamingBlobResponse(model, settings.getService(model))) {
+            return Collections.emptyMap();
+        }
+        switch (target) {
+            case NODE:
+                return MapUtils.of("sdkStreamMixin", writer -> {
+                    writer.addDependency(TypeScriptDependency.UTIL_STREAM_NODE);
+                    writer.addImport("sdkStreamMixin", "sdkStreamMixin",
+                            TypeScriptDependency.UTIL_STREAM_NODE.packageName);
+                    writer.write("sdkStreamMixin");
+                });
+            case BROWSER:
+                return MapUtils.of("sdkStreamMixin", writer -> {
+                    writer.addDependency(TypeScriptDependency.UTIL_STREAM_BROWSER);
+                    writer.addImport("sdkStreamMixin", "sdkStreamMixin",
+                            TypeScriptDependency.UTIL_STREAM_BROWSER.packageName);
+                    writer.write("sdkStreamMixin");
+                });
+            default:
+                return Collections.emptyMap();
+        }
+    }
+
+    private static boolean hasStreamingBlobResponse(Model model, ServiceShape serviceShape) {
+        TopDownIndex topDownIndex = TopDownIndex.of(model);
+        Set<OperationShape> operations = topDownIndex.getContainedOperations(serviceShape);
+        for (OperationShape operation : operations) {
+            StructureShape outputShape = model.expectShape(operation.getOutputShape()).asStructureShape().get();
+            for (MemberShape member : outputShape.members()) {
+                Shape shape = model.expectShape(member.getTarget());
+                if (shape instanceof BlobShape && shape.hasTrait(StreamingTrait.class)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
