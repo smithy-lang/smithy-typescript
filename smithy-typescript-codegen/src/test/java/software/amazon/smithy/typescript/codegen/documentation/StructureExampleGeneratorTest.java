3c35b7e545827bcf9ed5dfc832c8eb60a2addd37
George Fu <kuhe@users.noreply.github.com>
2023-03-28 15:30:35 +0000

    add recursive test for structural doc gen

--- smithy-typescript-codegen/src/test/java/software/amazon/smithy/typescript/codegen/documentation/StructureExampleGeneratorTest.java
+++ smithy-typescript-codegen/src/test/java/software/amazon/smithy/typescript/codegen/documentation/StructureExampleGeneratorTest.java
@@ -53,7 +53,36 @@ public class StructureExampleGeneratorTest {
     StructureShape structure = StructureShape.builder()
             .id("foo.bar#structure")
             .members(
-                    List.<MemberShape>of(memberForString, memberForList, memberForMap))
+                    List.<MemberShape>of(
+                            memberForString, memberForList, memberForMap,
+                            MemberShape.builder()
+                                    .id("foo.bar#structure$list2")
+                                    .target(list.getId())
+                                    .build(),
+                            MemberShape.builder()
+                                    .id("foo.bar#structure$list3")
+                                    .target(list.getId())
+                                    .build(),
+                            MemberShape.builder()
+                                    .id("foo.bar#structure$list4")
+                                    .target(list.getId())
+                                    .build(),
+                            MemberShape.builder()
+                                    .id("foo.bar#structure$list5")
+                                    .target(list.getId())
+                                    .build(),
+                            MemberShape.builder()
+                                    .id("foo.bar#structure$list6")
+                                    .target(list.getId())
+                                    .build(),
+                            MemberShape.builder()
+                                    .id("foo.bar#structure$list7")
+                                    .target(list.getId())
+                                    .build(),
+                            MemberShape.builder()
+                                    .id("foo.bar#structure$structure")
+                                    .target("foo.bar#structure")
+                                    .build()))
             .build();
 
     private Model model = Model.builder()
@@ -84,6 +113,34 @@ public class StructureExampleGeneratorTest {
                           ],
                           map: { // map
                             "<keys>": "STRING_VALUE",
+                          },
+                          list2: [
+                            "STRING_VALUE",
+                          ],
+                          list3: [
+                            "STRING_VALUE",
+                          ],
+                          list4: [
+                            "STRING_VALUE",
+                          ],
+                          list5: [
+                            "STRING_VALUE",
+                          ],
+                          list6: "<list>",
+                          list7: "<list>",
+                          structure: {
+                            string: "STRING_VALUE",
+                            list: "<list>",
+                            map: {
+                              "<keys>": "STRING_VALUE",
+                            },
+                            list2: "<list>",
+                            list3: "<list>",
+                            list4: "<list>",
+                            list5: "<list>",
+                            list6: "<list>",
+                            list7: "<list>",
+                            structure: "<structure>",
                           },
                         };"""));
     }
