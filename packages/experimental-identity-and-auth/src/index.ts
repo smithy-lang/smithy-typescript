d23eecabed0dd5eda1cedb9d893283d5ed9bc9da
Steven Yuan <yuasteve@amazon.com>
2023-08-17 14:38:03 -0700

    feat(experimentalIdentityAndAuth): add `@httpApiKeyAuth` TypeScript code

--- packages/experimental-identity-and-auth/src/index.ts
+++ packages/experimental-identity-and-auth/src/index.ts
@@ -2,3 +2,5 @@ export * from "./IdentityProviderConfiguration";
 export * from "./HttpAuthScheme";
 export * from "./HttpSigner";
 export * from "./noAuth";
+export * from "./apiKeyIdentity";
+export * from "./httpApiKeyAuth";
