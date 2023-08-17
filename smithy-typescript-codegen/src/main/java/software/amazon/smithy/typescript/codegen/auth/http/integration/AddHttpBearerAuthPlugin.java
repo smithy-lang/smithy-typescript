d30d95d823ad93197aa890a54f0533861fad65a0
Steven Yuan <yuasteve@amazon.com>
2023-08-17 14:42:25 -0700

    feat(experimentalIdentityAndAuth): Add generic `@httpBearerAuth` support
    Registers the `@httpBearerAuth` scheme.

---
+++ smithy-typescript-codegen/src/main/java/software/amazon/smithy/typescript/codegen/auth/http/integration/AddHttpBearerAuthPlugin.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package software.amazon.smithy.typescript.codegen.auth.http.integration;
+
+import java.util.Optional;
+import software.amazon.smithy.model.traits.HttpBearerAuthTrait;
+import software.amazon.smithy.typescript.codegen.ApplicationProtocol;
+import software.amazon.smithy.typescript.codegen.ConfigField;
+import software.amazon.smithy.typescript.codegen.LanguageTarget;
+import software.amazon.smithy.typescript.codegen.TypeScriptDependency;
+import software.amazon.smithy.typescript.codegen.TypeScriptSettings;
+import software.amazon.smithy.typescript.codegen.auth.http.HttpAuthScheme;
+import software.amazon.smithy.utils.SmithyInternalApi;
+
+/**
+ * Support for @httpBearerAuth.
+ *
+ * This is the experimental behavior for `experimentalIdentityAndAuth`.
+ */
+@SmithyInternalApi
+public final class AddHttpBearerAuthPlugin implements HttpAuthTypeScriptIntegration {
+
+    /**
+     * Integration should only be used if `experimentalIdentityAndAuth` flag is true.
+     */
+    @Override
+    public boolean matchesSettings(TypeScriptSettings settings) {
+        return settings.getExperimentalIdentityAndAuth();
+    }
+
+    @Override
+    public Optional<HttpAuthScheme> getHttpAuthScheme() {
+        return Optional.of(HttpAuthScheme.builder()
+                .schemeId(HttpBearerAuthTrait.ID)
+                .applicationProtocol(ApplicationProtocol.createDefaultHttpApplicationProtocol())
+                .addConfigField(new ConfigField("token", w -> {
+                    w.addDependency(TypeScriptDependency.EXPERIMENTAL_IDENTITY_AND_AUTH);
+                    w.addImport("TokenIdentity", null,
+                        TypeScriptDependency.EXPERIMENTAL_IDENTITY_AND_AUTH);
+                    w.addImport("TokenIdentityProvider", null,
+                        TypeScriptDependency.EXPERIMENTAL_IDENTITY_AND_AUTH);
+                    w.write("TokenIdentity | TokenIdentityProvider");
+                }, w -> w.write("The token used to authenticate requests.")))
+                .putDefaultIdentityProvider(LanguageTarget.SHARED, w -> {
+                    w.write("async () => { throw new Error(\"`token` is missing\"); }");
+                })
+                .putDefaultSigner(LanguageTarget.SHARED, w -> {
+                    w.addDependency(TypeScriptDependency.EXPERIMENTAL_IDENTITY_AND_AUTH);
+                    w.addImport("HttpBearerAuthSigner", null,
+                        TypeScriptDependency.EXPERIMENTAL_IDENTITY_AND_AUTH);
+                    w.write("new HttpBearerAuthSigner()");
+                })
+                .build());
+    }
+}
