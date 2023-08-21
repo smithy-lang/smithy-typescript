4571d59c86adefec92a4b7cc65a8b48642b1acb2
George Fu <kuhe@users.noreply.github.com>
2023-08-21 12:58:56 -0400

    fix(util-body-length-node): calculateBodyLength for Readable stream with range (#862)

--- packages/util-body-length-node/src/calculateBodyLength.ts
+++ packages/util-body-length-node/src/calculateBodyLength.ts
@@ -14,6 +14,8 @@ export const calculateBodyLength = (body: any): number | undefined => {
     return body.byteLength;
   } else if (typeof body.size === "number") {
     return body.size;
+  } else if (typeof body.start === "number" && typeof body.end === "number") {
+    return body.end + 1 - body.start;
   } else if (typeof body.path === "string" || Buffer.isBuffer(body.path)) {
     // handles fs readable streams
     return lstatSync(body.path).size;
