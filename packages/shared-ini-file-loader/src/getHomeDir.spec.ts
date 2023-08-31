cf00ecc6ffcc87680c85b39947be7ff7c7cc08c7
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-08-31 22:59:35 +0000

    test: calls for homedir and process.geteuid

--- packages/shared-ini-file-loader/src/getHomeDir.spec.ts
+++ packages/shared-ini-file-loader/src/getHomeDir.spec.ts
@@ -17,7 +17,6 @@ describe(getHomeDir.name, () => {
 
   beforeEach(() => {
     (homedir as jest.Mock).mockReturnValue(mockHomeDir);
-    jest.spyOn(process, "geteuid").mockReturnValue(mockUid);
     process.env = {
       ...OLD_ENV,
       HOME: mockHOME,
@@ -34,16 +33,22 @@ describe(getHomeDir.name, () => {
 
   it("returns value in process.env.HOME first", () => {
     expect(getHomeDir()).toEqual(mockHOME);
+    expect(homedir).not.toHaveBeenCalled();
   });
 
   it("returns value in process.env.USERPROFILE second", () => {
     process.env = { ...process.env, HOME: undefined };
     expect(getHomeDir()).toEqual(mockUSERPROFILE);
+    expect(homedir).not.toHaveBeenCalled();
   });
 
   describe("returns value in HOMEPATH third", () => {
     beforeEach(() => {
       process.env = { ...process.env, HOME: undefined, USERPROFILE: undefined };
+    });
+
+    afterEach(() => {
+      expect(homedir).not.toHaveBeenCalled();
     });
 
     it("uses value in process.env.HOMEDRIVE if it's set", () => {
@@ -57,8 +62,11 @@ describe(getHomeDir.name, () => {
   });
 
   it("returns value from homedir fourth", () => {
+    const processGeteuidSpy = jest.spyOn(process, "geteuid").mockReturnValue(mockUid);
     process.env = { ...process.env, HOME: undefined, USERPROFILE: undefined, HOMEPATH: undefined };
     expect(getHomeDir()).toEqual(mockHomeDir);
+    expect(homedir).toHaveBeenCalledTimes(1);
+    expect(processGeteuidSpy).toHaveBeenCalledTimes(1);
   });
 
   describe("makes one homedir call irrespective of getHomeDir calls", () => {
@@ -79,22 +87,27 @@ describe(getHomeDir.name, () => {
     };
 
     describe("when geteuid is available", () => {
-      it.each([10, 100, 1000, 10000])("calls: %d ", testSingleHomeDirCall);
+      it.each([10, 100, 1000, 10000])("calls: %d ", (num: number) => {
+        const processGeteuidSpy = jest.spyOn(process, "geteuid").mockReturnValue(mockUid);
+        expect(processGeteuidSpy).not.toHaveBeenCalled();
+        testSingleHomeDirCall(num);
+        expect(processGeteuidSpy).toHaveBeenCalledTimes(num);
+      });
     });
 
     describe("when geteuid is not available", () => {
       const OLD_GETEUID = process.geteuid;
 
-      beforeEach(() => {
+      beforeAll(() => {
         // @ts-ignore Type 'undefined' is not assignable to type '() => number'.
         process.geteuid = undefined;
       });
 
-      it.each([10, 100, 1000, 10000])("calls: %d ", testSingleHomeDirCall);
-
-      afterEach(() => {
+      afterAll(() => {
         process.geteuid = OLD_GETEUID;
       });
+
+      it.each([10, 100, 1000, 10000])("calls: %d ", testSingleHomeDirCall);
     });
   });
 
@@ -102,12 +115,14 @@ describe(getHomeDir.name, () => {
     it.each([2, 10, 100])("calls: %d ", (num: number) => {
       jest.isolateModules(() => {
         const { getHomeDir } = require("./getHomeDir");
+        const processGeteuidSpy = jest.spyOn(process, "geteuid").mockReturnValue(mockUid);
         for (let i = 0; i < num; i++) {
           jest.spyOn(process, "geteuid").mockReturnValueOnce(mockUid + i);
         }
         process.env = { ...process.env, HOME: undefined, USERPROFILE: undefined, HOMEPATH: undefined };
 
         expect(homedir).not.toHaveBeenCalled();
+        expect(processGeteuidSpy).not.toHaveBeenCalled();
         const homeDirArr = Array(num)
           .fill(num)
           .map(() => getHomeDir());
@@ -115,12 +130,15 @@ describe(getHomeDir.name, () => {
 
         // There is num homedir calls as each call returns different UID
         expect(homedir).toHaveBeenCalledTimes(num);
+        expect(processGeteuidSpy).toHaveBeenCalledTimes(num);
 
         const homeDir = getHomeDir();
         expect(homeDir).toStrictEqual(mockHomeDir);
 
         // No extra calls made to homedir, as mockUid is same as the first call.
         expect(homedir).toHaveBeenCalledTimes(num);
+        // Extra call was made to geteuid to get the same UID as the first call.
+        expect(processGeteuidSpy).toHaveBeenCalledTimes(num + 1);
       });
     });
   });
