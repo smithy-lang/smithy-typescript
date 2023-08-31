d803338acfea741ed442a337b90a36d9972312fd
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-08-31 22:43:35 +0000

    test: number of homedir calls same as UIDs

--- packages/shared-ini-file-loader/src/getHomeDir.spec.ts
+++ packages/shared-ini-file-loader/src/getHomeDir.spec.ts
@@ -6,6 +6,7 @@ import { getHomeDir } from "./getHomeDir";
 jest.mock("os");
 
 describe(getHomeDir.name, () => {
+  const mockUid = 1;
   const mockHOME = "mockHOME";
   const mockUSERPROFILE = "mockUSERPROFILE";
   const mockHOMEPATH = "mockHOMEPATH";
@@ -16,6 +17,7 @@ describe(getHomeDir.name, () => {
 
   beforeEach(() => {
     (homedir as jest.Mock).mockReturnValue(mockHomeDir);
+    jest.spyOn(process, "geteuid").mockReturnValue(mockUid);
     process.env = {
       ...OLD_ENV,
       HOME: mockHOME,
@@ -59,8 +61,8 @@ describe(getHomeDir.name, () => {
     expect(getHomeDir()).toEqual(mockHomeDir);
   });
 
-  describe("makes one homedir call per user filepath irrespective of getHomeDir calls", () => {
-    it.each([10, 100, 1000, 10000])("parallel calls: %d ", (num: number) => {
+  describe("makes one homedir call per UID irrespective of getHomeDir calls", () => {
+    it.each([10, 100, 1000, 10000])("calls: %d ", (num: number) => {
       jest.isolateModules(() => {
         const { getHomeDir } = require("./getHomeDir");
         process.env = { ...process.env, HOME: undefined, USERPROFILE: undefined, HOMEPATH: undefined };
@@ -71,8 +73,35 @@ describe(getHomeDir.name, () => {
           .map(() => getHomeDir());
         expect(homeDirArr).toStrictEqual(Array(num).fill(mockHomeDir));
 
-        // There is one homedir call even through getHomeDir is called in parallel num times.
+        // There is one homedir call even through getHomeDir is called num times.
         expect(homedir).toHaveBeenCalledTimes(1);
+      });
+    });
+  });
+
+  describe("makes multiple homedir calls with based on UIDs", () => {
+    it.each([2, 10, 100])("calls: %d ", (num: number) => {
+      jest.isolateModules(() => {
+        const { getHomeDir } = require("./getHomeDir");
+        for (let i = 0; i < num; i++) {
+          jest.spyOn(process, "geteuid").mockReturnValueOnce(mockUid + i);
+        }
+        process.env = { ...process.env, HOME: undefined, USERPROFILE: undefined, HOMEPATH: undefined };
+
+        expect(homedir).not.toHaveBeenCalled();
+        const homeDirArr = Array(num)
+          .fill(num)
+          .map(() => getHomeDir());
+        expect(homeDirArr).toStrictEqual(Array(num).fill(mockHomeDir));
+
+        // There is num homedir calls as each call returns different UID
+        expect(homedir).toHaveBeenCalledTimes(num);
+
+        const homeDir = getHomeDir();
+        expect(homeDir).toStrictEqual(mockHomeDir);
+
+        // No extra calls made to homedir, as mockUid is same as the first call.
+        expect(homedir).toHaveBeenCalledTimes(num);
       });
     });
   });
