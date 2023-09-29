e053a7c8f3502676b389019a19a69b1624cf37a6
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-09-29 14:49:37 +0000

    test(fetch-http-handler): basic fetch request

--- packages/fetch-http-handler/src/fetch-http-handler.browser.spec.ts
+++ packages/fetch-http-handler/src/fetch-http-handler.browser.spec.ts
@@ -1,11 +1,24 @@
+import { HttpRequest } from "@smithy/protocol-http";
+
 import { FetchHttpHandler } from "./fetch-http-handler";
 
 describe(FetchHttpHandler.name, () => {
-  it("calls request without mode included in requestOptions", (done) => {
-    const fetchHttpHandler = new FetchHttpHandler();
-    const spy = spyOn(window, "Request");
-    fetchHttpHandler.handle({} as any, {});
-    expect(spy.calls.argsFor(0)[1].mode).toEqual(undefined);
-    done();
+  const getMockHttpRequest = (): HttpRequest => new HttpRequest({ hostname: "example.com" });
+
+  describe("fetch", () => {
+    it("sends basic fetch request", (done) => {
+      const fetchHttpHandler = new FetchHttpHandler();
+      const winReqSpy = spyOn(window, "Request");
+
+      const mockHttpRequest = getMockHttpRequest();
+      fetchHttpHandler.handle(mockHttpRequest);
+
+      const expectedUrl = `${mockHttpRequest.protocol}//${mockHttpRequest.hostname}${mockHttpRequest.path}`;
+      const requestArgs = winReqSpy.calls.argsFor(0);
+      expect(requestArgs[0]).toEqual(expectedUrl);
+      expect(requestArgs[1].method).toEqual(mockHttpRequest.method);
+
+      done();
+    });
   });
 });
