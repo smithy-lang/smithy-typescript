121254a1a2b73106fe4655707f5e792c539a860a
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-09-29 15:08:37 +0000

    test(fetch-http-handler): body with GET/HEAD

--- packages/fetch-http-handler/src/fetch-http-handler.browser.spec.ts
+++ packages/fetch-http-handler/src/fetch-http-handler.browser.spec.ts
@@ -3,14 +3,15 @@ import { HttpRequest } from "@smithy/protocol-http";
 import { FetchHttpHandler } from "./fetch-http-handler";
 
 describe(FetchHttpHandler.name, () => {
-  const getMockHttpRequest = (): HttpRequest => new HttpRequest({ hostname: "example.com" });
+  const getMockHttpRequest = ({ method, body }: { method?: string; body?: any }): HttpRequest =>
+    new HttpRequest({ method, hostname: "example.com", body });
 
   describe("fetch", () => {
     it("sends basic fetch request", (done) => {
       const fetchHttpHandler = new FetchHttpHandler();
       const winReqSpy = spyOn(window, "Request");
 
-      const mockHttpRequest = getMockHttpRequest();
+      const mockHttpRequest = getMockHttpRequest({});
       fetchHttpHandler.handle(mockHttpRequest);
 
       const expectedUrl = `${mockHttpRequest.protocol}//${mockHttpRequest.hostname}${mockHttpRequest.path}`;
@@ -20,5 +21,21 @@ describe(FetchHttpHandler.name, () => {
 
       done();
     });
+
+    for (const method of ["GET", "HEAD"]) {
+      it(`sets body to undefined when method: '${method}'`, (done) => {
+        const fetchHttpHandler = new FetchHttpHandler();
+        const winReqSpy = spyOn(window, "Request");
+
+        const mockHttpRequest = getMockHttpRequest({ method, body: "test" });
+        fetchHttpHandler.handle(mockHttpRequest);
+
+        const requestArgs = winReqSpy.calls.argsFor(0);
+        expect(requestArgs[1].method).toEqual(mockHttpRequest.method);
+        expect(requestArgs[1].body).toEqual(undefined);
+
+        done();
+      });
+    }
   });
 });
