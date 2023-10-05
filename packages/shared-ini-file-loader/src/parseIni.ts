60e88afe9349fa540c54741a1b17c608aa6adbd5
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2023-10-05 13:51:55 -0700

    Read values from main section when parsing INI files (#986)

--- packages/shared-ini-file-loader/src/parseIni.ts
+++ packages/shared-ini-file-loader/src/parseIni.ts
@@ -4,29 +4,33 @@ const profileNameBlockList = ["__proto__", "profile __proto__"];
 
 export const parseIni = (iniData: string): ParsedIniData => {
   const map: ParsedIniData = {};
+
   let currentSection: string | undefined;
+  let currentSubSection: string | undefined;
 
   for (let line of iniData.split(/\r?\n/)) {
     line = line.split(/(^|\s)[;#]/)[0].trim(); // remove comments and trim
     const isSection: boolean = line[0] === "[" && line[line.length - 1] === "]";
     if (isSection) {
+      currentSubSection = undefined;
       currentSection = line.substring(1, line.length - 1);
       if (profileNameBlockList.includes(currentSection)) {
         throw new Error(`Found invalid profile name "${currentSection}"`);
       }
     } else if (currentSection) {
       const indexOfEqualsSign = line.indexOf("=");
-      const start = 0;
-      const end: number = line.length - 1;
-      const isAssignment: boolean =
-        indexOfEqualsSign !== -1 && indexOfEqualsSign !== start && indexOfEqualsSign !== end;
-      if (isAssignment) {
+      if (![0, -1].includes(indexOfEqualsSign)) {
         const [name, value]: [string, string] = [
           line.substring(0, indexOfEqualsSign).trim(),
           line.substring(indexOfEqualsSign + 1).trim(),
         ];
-        map[currentSection] = map[currentSection] || {};
-        map[currentSection][name] = value;
+        if (value === "") {
+          currentSubSection = name;
+        } else if (currentSubSection === undefined) {
+          // ToDo: populate subsection in future PR, when IniSection is updated to support subsections.
+          map[currentSection] = map[currentSection] || {};
+          map[currentSection][name] = value;
+        }
       }
     }
   }
