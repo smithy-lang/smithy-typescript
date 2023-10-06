75ac7828e6813cc7826f2bfe238993e259a1098f
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-10-06 19:55:04 +0000

    Check for presence of CONFIG_PREFIX_SEPARATOR in getSsoSessionData

--- packages/shared-ini-file-loader/src/getSsoSessionData.ts
+++ packages/shared-ini-file-loader/src/getSsoSessionData.ts
@@ -9,6 +9,6 @@ import { CONFIG_PREFIX_SEPARATOR } from "./loadSharedConfigFiles";
 export const getSsoSessionData = (data: ParsedIniData): ParsedIniData =>
   Object.entries(data)
     // filter out non sso-session keys
-    .filter(([key]) => key.startsWith(IniSectionType.SSO_SESSION))
+    .filter(([key]) => key.startsWith(IniSectionType.SSO_SESSION + CONFIG_PREFIX_SEPARATOR))
     // replace sso-session key with sso-session name
     .reduce((acc, [key, value]) => ({ ...acc, [key.split(CONFIG_PREFIX_SEPARATOR)[1]]: value }), {});
