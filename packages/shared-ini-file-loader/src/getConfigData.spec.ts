985b8df16d56fbee009d8f4adc04e049764cca42
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-10-06 19:51:23 +0000

    Add utility getConfigData

---
+++ packages/shared-ini-file-loader/src/getConfigData.spec.ts
@@ -0,0 +1,73 @@
+import { IniSectionType } from "@smithy/types";
+
+import { getConfigData } from "./getConfigData";
+import { CONFIG_PREFIX_SEPARATOR } from "./loadSharedConfigFiles";
+
+describe(getConfigData.name, () => {
+  it("returns empty for no data", () => {
+    expect(getConfigData({})).toStrictEqual({});
+  });
+
+  it("returns default profile if present", () => {
+    const mockInput = { default: { key: "value" } };
+    expect(getConfigData(mockInput)).toStrictEqual(mockInput);
+  });
+
+  it("skips profiles without prefix profile", () => {
+    const mockInput = { test: { key: "value" } };
+    expect(getConfigData(mockInput)).toStrictEqual({});
+  });
+
+  it.each([IniSectionType.SSO_SESSION, IniSectionType.SERVICES])("includes sections with '%s' prefix", (prefix) => {
+    const mockInput = { [[prefix, "test"].join(CONFIG_PREFIX_SEPARATOR)]: { key: "value" } };
+    expect(getConfigData(mockInput)).toStrictEqual(mockInput);
+  });
+
+  describe("normalizes profile names", () => {
+    const getMockProfileData = (profileName: string) =>
+      [1, 2, 3]
+        .map((num) => [`key_${profileName}_${num}`, `value_${profileName}_${num}`])
+        .reduce((acc, [key, value]) => ({ ...acc, [key]: value }), {});
+
+    const getMockOutput = (profileNames: string[]) =>
+      profileNames.reduce((acc, profileName) => ({ ...acc, [profileName]: getMockProfileData(profileName) }), {});
+
+    const getMockInput = (mockOutput: Record<string, Record<string, string>>) =>
+      Object.entries(mockOutput).reduce(
+        (acc, [key, value]) => ({ ...acc, [[IniSectionType.PROFILE, key].join(CONFIG_PREFIX_SEPARATOR)]: value }),
+        {}
+      );
+
+    it("single profile", () => {
+      const mockOutput = getMockOutput(["one"]);
+      const mockInput = getMockInput(mockOutput);
+      expect(getConfigData(mockInput)).toStrictEqual(mockOutput);
+    });
+
+    it("two profiles", () => {
+      const mockOutput = getMockOutput(["one", "two"]);
+      const mockInput = getMockInput(mockOutput);
+      expect(getConfigData(mockInput)).toStrictEqual(mockOutput);
+    });
+
+    it("three profiles", () => {
+      const mockOutput = getMockOutput(["one", "two", "three"]);
+      const mockInput = getMockInput(mockOutput);
+      expect(getConfigData(mockInput)).toStrictEqual(mockOutput);
+    });
+
+    it("with default", () => {
+      const defaultInput = { default: { key: "value" } };
+      const mockOutput = getMockOutput(["one"]);
+      const mockInput = getMockInput(mockOutput);
+      expect(getConfigData({ ...defaultInput, ...mockInput })).toStrictEqual({ ...defaultInput, ...mockOutput });
+    });
+
+    it("with profileName without prefix", () => {
+      const profileWithPrefix = { test: { key: "value" } };
+      const mockOutput = getMockOutput(["one"]);
+      const mockInput = getMockInput(mockOutput);
+      expect(getConfigData({ ...profileWithPrefix, ...mockInput })).toStrictEqual(mockOutput);
+    });
+  });
+});
---
+++ packages/shared-ini-file-loader/src/getConfigData.ts
@@ -0,0 +1,32 @@
+import { IniSectionType, ParsedIniData } from "@smithy/types";
+
+import { CONFIG_PREFIX_SEPARATOR } from "./loadSharedConfigFiles";
+
+/**
+ * Returns the config data from parsed ini data.
+ * * Returns data for `default`
+ * * Returns profile name without prefix.
+ * * Returns non-profiles as is.
+ */
+export const getConfigData = (data: ParsedIniData): ParsedIniData =>
+  Object.entries(data)
+    // filter out
+    .filter(([key]) => {
+      const sections = key.split(CONFIG_PREFIX_SEPARATOR);
+      if (sections.length === 2 && Object.values(IniSectionType).includes(sections[0] as IniSectionType)) {
+        return true;
+      }
+      return false;
+    })
+    // replace profile prefix, if present.
+    .reduce(
+      (acc, [key, value]) => {
+        const updatedKey = key.startsWith(IniSectionType.PROFILE) ? key.split(CONFIG_PREFIX_SEPARATOR)[1] : key;
+        acc[updatedKey] = value;
+        return acc;
+      },
+      {
+        // Populate default profile, if present.
+        ...(data.default && { default: data.default }),
+      }
+    );
