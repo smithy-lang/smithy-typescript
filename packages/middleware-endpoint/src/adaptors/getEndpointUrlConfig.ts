4ebfd1ad29e287978ea0eac071b5e8fd0ce61fc3
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-10-09 20:49:50 +0000

    Support reading endpoints from env when serviceId contains spaces

--- packages/middleware-endpoint/src/adaptors/getEndpointUrlConfig.ts
+++ packages/middleware-endpoint/src/adaptors/getEndpointUrlConfig.ts
@@ -7,7 +7,7 @@ const CONFIG_ENDPOINT_URL = "endpoint_url";
 export const getEndpointUrlConfig = (serviceId: string): LoadedConfigSelectors<string | undefined> => ({
   environmentVariableSelector: (env) => {
     // The value provided by a service-specific environment variable.
-    const serviceEndpointUrlSections = [ENV_ENDPOINT_URL, serviceId.toUpperCase()];
+    const serviceEndpointUrlSections = [ENV_ENDPOINT_URL, ...serviceId.split(" ").map((w) => w.toUpperCase())];
     const serviceEndpointUrl = env[serviceEndpointUrlSections.join("_")];
     if (serviceEndpointUrl) return serviceEndpointUrl;
 
