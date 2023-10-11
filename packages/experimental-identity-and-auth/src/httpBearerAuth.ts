bd219665de4a7c995e4f20dff90aedf338e64980
Steven Yuan <yuasteve@amazon.com>
2023-10-11 12:54:36 -0700

    fix(experimentalIdentityAndAuth): add strict check for `token` in `HttpBearerAuthSigner`

--- packages/experimental-identity-and-auth/src/httpBearerAuth.ts
+++ packages/experimental-identity-and-auth/src/httpBearerAuth.ts
@@ -14,6 +14,9 @@ export class HttpBearerAuthSigner implements HttpSigner {
     signingProperties: Record<string, any>
   ): Promise<IHttpRequest> {
     const clonedRequest = httpRequest.clone();
+    if (!identity.token) {
+      throw new Error("request could not be signed with `token` since the `token` is not defined");
+    }
     clonedRequest.headers["Authorization"] = `Bearer ${identity.token}`;
     return clonedRequest;
   }
