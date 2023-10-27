99322accc98e52522eaa5ef99b057ee88df91daf
Steven Yuan <yuasteve@amazon.com>
2023-10-26 17:09:55 -0700

    Update TypeScript codegen plugins to follow idiomatic plugin names
    See: https://smithy.io/2.0/guides/building-codegen/configuring-the-generator.html#how-to-name-codegen-plugins

--- smithy-typescript-codegen/src/main/java/software/amazon/smithy/typescript/codegen/TypeScriptClientCodegenPlugin.java
+++ smithy-typescript-codegen/src/main/java/software/amazon/smithy/typescript/codegen/TypeScriptClientCodegenPlugin.java
@@ -1,20 +1,13 @@
 /*
- * Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License").
- * You may not use this file except in compliance with the License.
- * A copy of the License is located at
- *
- *  http://aws.amazon.com/apache2.0
- *
- * or in the "license" file accompanying this file. This file is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 package software.amazon.smithy.typescript.codegen;
 
+import java.util.ServiceLoader;
+import java.util.ServiceLoader.Provider;
+import java.util.logging.Logger;
 import software.amazon.smithy.build.PluginContext;
 import software.amazon.smithy.build.SmithyBuildPlugin;
 import software.amazon.smithy.codegen.core.directed.CodegenDirector;
@@ -22,14 +15,15 @@ import software.amazon.smithy.typescript.codegen.integration.TypeScriptIntegrati
 import software.amazon.smithy.utils.SmithyInternalApi;
 
 /**
- * Plugin to trigger TypeScript SSDK code generation.
+ * Plugin to trigger TypeScript client code generation.
  */
 @SmithyInternalApi
-public class TypeScriptServerCodegenPlugin implements SmithyBuildPlugin {
+public class TypeScriptClientCodegenPlugin implements SmithyBuildPlugin {
+    private static final Logger LOGGER = Logger.getLogger(TypeScriptClientCodegenPlugin.class.getName());
 
     @Override
     public String getName() {
-        return "typescript-server-codegen";
+        return "typescript-client-codegen";
     }
 
     @Override
@@ -48,8 +42,24 @@ public class TypeScriptServerCodegenPlugin implements SmithyBuildPlugin {
 
         // Create the TypeScriptSettings object from the plugin settings.
         TypeScriptSettings settings = TypeScriptSettings.from(context.getModel(), context.getSettings(),
-                TypeScriptSettings.ArtifactType.SSDK);
+                TypeScriptSettings.ArtifactType.CLIENT);
         runner.settings(settings);
+
+        // Only add integrations if the integrations match the settings
+        // This uses {@link TypeScriptIntegration#matchesSettings}, which is a
+        // Smithy internal API. This may be removed at any point.
+        runner.integrationFinder(() ->
+            () -> ServiceLoader.load(TypeScriptIntegration.class, CodegenDirector.class.getClassLoader())
+                .stream()
+                .map(Provider::get)
+                .filter(integration -> {
+                    boolean matchesSettings = integration.matchesSettings(settings);
+                    if (!matchesSettings) {
+                        LOGGER.info(() -> "Skipping TypeScript integration based on settings: " + integration.name());
+                    }
+                    return matchesSettings;
+                })
+                .iterator());
 
         runner.service(settings.getService());
 
---
+++ smithy-typescript-codegen/src/main/java/software/amazon/smithy/typescript/codegen/TypeScriptSSDKCodegenPlugin.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package software.amazon.smithy.typescript.codegen;
+
+import software.amazon.smithy.utils.SmithyInternalApi;
+
+/**
+ * Plugin to trigger TypeScript SSDK code generation.
+ */
+@SmithyInternalApi
+@Deprecated
+public class TypeScriptSSDKCodegenPlugin extends TypeScriptServerCodegenPlugin {
+    @Override
+    public String getName() {
+        return "typescript-ssdk-codegen";
+    }
+}
