be21d1769ff6c512907f7d0fa6685e9393499bd6
George Fu <kuhe@users.noreply.github.com>
2023-12-01 16:57:17 +0000

    formatting

--- packages/middleware-retry/src/isStreamingPayload/isStreamingPayload.ts
+++ packages/middleware-retry/src/isStreamingPayload/isStreamingPayload.ts
@@ -4,6 +4,6 @@ import { Readable } from "stream";
 /**
  * @internal
  */
-export const isStreamingPayload = (request: HttpRequest): boolean => 
+export const isStreamingPayload = (request: HttpRequest): boolean =>
   request?.body instanceof Readable ||
   (typeof ReadableStream !== "undefined" && request?.body instanceof ReadableStream);
