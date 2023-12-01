3b2f9a543152a32b7bcfb7590849a4c2b8a73579
George Fu <kuhe@users.noreply.github.com>
2023-12-01 11:56:51 -0500

    format packages/middleware-retry/src/isStreamingPayload/isStreamingPayload.browser.ts
    Co-authored-by: Trivikram Kamat <16024985+trivikr@users.noreply.github.com>

--- packages/middleware-retry/src/isStreamingPayload/isStreamingPayload.browser.ts
+++ packages/middleware-retry/src/isStreamingPayload/isStreamingPayload.browser.ts
@@ -3,6 +3,4 @@ import type { HttpRequest } from "@smithy/protocol-http";
 /**
  * @internal
  */
-export const isStreamingPayload = (request: HttpRequest): boolean => {
-  return request?.body instanceof ReadableStream;
-};
+export const isStreamingPayload = (request: HttpRequest): boolean => request?.body instanceof ReadableStream;
