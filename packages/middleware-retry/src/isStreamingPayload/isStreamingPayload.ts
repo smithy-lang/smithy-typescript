c2b4af02d0103eb01755c32d7d75a9496350917e
George Fu <kuhe@users.noreply.github.com>
2023-12-01 11:56:59 -0500

    format packages/middleware-retry/src/isStreamingPayload/isStreamingPayload.ts
    Co-authored-by: Trivikram Kamat <16024985+trivikr@users.noreply.github.com>

--- packages/middleware-retry/src/isStreamingPayload/isStreamingPayload.ts
+++ packages/middleware-retry/src/isStreamingPayload/isStreamingPayload.ts
@@ -4,9 +4,6 @@ import { Readable } from "stream";
 /**
  * @internal
  */
-export const isStreamingPayload = (request: HttpRequest): boolean => {
-  return (
-    request?.body instanceof Readable ||
-    (typeof ReadableStream !== "undefined" && request?.body instanceof ReadableStream)
-  );
-};
+export const isStreamingPayload = (request: HttpRequest): boolean => 
+  request?.body instanceof Readable ||
+  (typeof ReadableStream !== "undefined" && request?.body instanceof ReadableStream);
