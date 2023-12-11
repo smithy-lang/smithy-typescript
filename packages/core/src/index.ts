3fc08d32dfc4fe668a50645ed0722c1bf49856b5
George Fu <kuhe@users.noreply.github.com>
2023-12-11 16:45:21 +0000

    feat: add requestBuilder

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,3 +3,4 @@ export * from "./middleware-http-signing";
 export * from "./util-identity-and-auth";
 export * from "./getSmithyContext";
 export * from "./normalizeProvider";
+export * from "./protocols/requestBuilder";
