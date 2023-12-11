75cbb3e8892f15eea0d625c5a153533b5c7f162d
George Fu <kuhe@users.noreply.github.com>
2023-12-11 15:25:59 -0500

    feat: add requestBuilder (#1107)

---
+++ .changeset/silent-pianos-cheer.md
@@ -0,0 +1,5 @@
+---
+"@smithy/core": minor
+---
+
+add requestBuilder
---
+++ packages/core/src/protocols/requestBuilder.spec.ts
@@ -0,0 +1,53 @@
+import { HttpRequest } from "@smithy/protocol-http";
+
+import { requestBuilder } from "./requestBuilder";
+
+describe(requestBuilder.name, () => {
+  it("can build requests", async () => {
+    expect(
+      await requestBuilder(
+        {
+          Key: "MyKey",
+          Bucket: "MyBucket",
+        },
+        {
+          endpoint: async () => {
+            return {
+              hostname: "localhost",
+              protocol: "https",
+              port: 8080,
+              path: "/a",
+            };
+          },
+        } as any
+      )
+        .bp("/{Key+}")
+        .p("Bucket", () => "MyBucket", "{Bucket}", false)
+        .p("Key", () => "MyKey", "{Key+}", false)
+        .m("PUT")
+        .h({
+          "my-header": "my-header-value",
+        })
+        .q({
+          "my-query": "my-query-value",
+        })
+        .b("test-body")
+        .build()
+    ).toEqual(
+      new HttpRequest({
+        protocol: "https",
+        hostname: "localhost",
+        port: 8080,
+        method: "PUT",
+        path: "/a/MyKey",
+        query: {
+          "my-query": "my-query-value",
+        },
+        headers: {
+          "my-header": "my-header-value",
+        },
+        body: "test-body",
+      })
+    );
+  });
+});
