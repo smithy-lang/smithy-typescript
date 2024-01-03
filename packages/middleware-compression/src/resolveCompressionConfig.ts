bab10ab6075a0e8a5c267dab8815c1d36bd6ea49
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2024-01-03 21:50:48 +0000

    test: resolveCompressionConfig.spec.ts

--- packages/middleware-compression/src/resolveCompressionConfig.ts
+++ packages/middleware-compression/src/resolveCompressionConfig.ts
@@ -8,5 +8,18 @@ import { CompressionInputConfig, CompressionResolvedConfig } from "./configurati
 export const resolveCompressionConfig = <T>(input: T & CompressionInputConfig): T & CompressionResolvedConfig => ({
   ...input,
   disableRequestCompression: normalizeProvider(input.disableRequestCompression),
-  requestMinCompressionSizeBytes: normalizeProvider(input.requestMinCompressionSizeBytes),
+  requestMinCompressionSizeBytes: async () => {
+    const requestMinCompressionSizeBytes = await normalizeProvider(input.requestMinCompressionSizeBytes)();
+
+    // The requestMinCompressionSizeBytes should be less than the upper limit for API Gateway
+    // https://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-openapi-minimum-compression-size.html
+    if (requestMinCompressionSizeBytes < 0 || requestMinCompressionSizeBytes > 10485760) {
+      throw new RangeError(
+        "The value for requestMinCompressionSizeBytes must be between 0 and 10485760 inclusive. " +
+          `The provided value ${requestMinCompressionSizeBytes} is outside this range."`
+      );
+    }
+
+    return requestMinCompressionSizeBytes;
+  },
 });
