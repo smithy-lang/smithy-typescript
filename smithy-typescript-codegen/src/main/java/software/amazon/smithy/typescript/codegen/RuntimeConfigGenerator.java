49b32d9c2a09b49a754aed26873deb051ab35c79
George Fu <kuhe@users.noreply.github.com>
2024-02-22 20:43:40 +0000

    unused import

--- smithy-typescript-codegen/src/main/java/software/amazon/smithy/typescript/codegen/RuntimeConfigGenerator.java
+++ smithy-typescript-codegen/src/main/java/software/amazon/smithy/typescript/codegen/RuntimeConfigGenerator.java
@@ -16,7 +16,6 @@
 package software.amazon.smithy.typescript.codegen;
 
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
