cf02f124e1c059cf134e0f0392c7f52e14d827f7
George Fu <kuhe@users.noreply.github.com>
2024-03-05 21:37:23 +0000

    fix: allow error deserializers to populate error response body

--- packages/middleware-serde/src/deserializerMiddleware.ts
+++ packages/middleware-serde/src/deserializerMiddleware.ts
@@ -33,6 +33,14 @@ export const deserializerMiddleware = <Input extends object, Output extends obje
       // only apply this to non-ServiceException.
       const hint = `Deserialization error: to see the raw response, inspect the hidden field {error}.$response on this object.`;
       error.message += "\n  " + hint;
+
+      if (typeof error.$responseBodyText !== "undefined") {
+        // if $responseBodyText was collected by the error parser, assign it to
+        // replace the response body, because it was consumed and is now empty.
+        if (error.$response) {
+          error.$response.body = error.$responseBodyText;
+        }
+      }
     }
 
     throw error;
