f6f91808f7410d36096b28c6f54cefe308c01244
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2024-05-09 00:49:56 +0000

    chore(server-node): remove unused variables

--- smithy-typescript-ssdk-libs/server-node/src/index.spec.ts
+++ smithy-typescript-ssdk-libs/server-node/src/index.spec.ts
@@ -30,7 +30,7 @@ beforeAll(async () => {
 let resToEnd: ServerResponse;
 
 function getRequest(options: RequestOptions & { body?: String }): Promise<[IncomingMessage, ServerResponse]> {
-  return new Promise((resolve, _) => {
+  return new Promise((resolve) => {
     promiseResolve = resolve;
     request({
       socketPath,
@@ -58,7 +58,7 @@ describe("convertRequest", () => {
     resToEnd?.end();
   });
   it("converts a simple GET / correctly", async () => {
-    const [req, _] = await getRequest({
+    const [req] = await getRequest({
       host: "example.com",
       path: "/",
     });
@@ -76,7 +76,7 @@ describe("convertRequest", () => {
     expect(await streamToString(convertedReq.body)).toEqual("");
   });
   it("converts a POST with query string correctly", async () => {
-    const [req, _] = await getRequest({
+    const [req] = await getRequest({
       method: "POST",
       host: "example.com",
       path: "/some/endpoint?q=hello&a=world",
@@ -100,7 +100,7 @@ describe("convertRequest", () => {
     expect(await streamToString(convertedReq.body)).toEqual("hello");
   });
   it("converts OPTIONS CORS requests", async () => {
-    const [req, _] = await getRequest({
+    const [req] = await getRequest({
       method: "OPTIONS",
       host: "example.com",
       path: "/some/resource",
