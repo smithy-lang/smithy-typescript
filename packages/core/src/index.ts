b226bec963d947c31811f519860ae5902b044ab1
George Fu <kuhe@users.noreply.github.com>
2024-05-14 17:22:28 +0000

    chore(core): move core components to core submodules

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,7 +1,23 @@
-export * from "./middleware-http-auth-scheme";
-export * from "./middleware-http-signing";
-export * from "./util-identity-and-auth";
+/**
+ * Submodules annotated with "Legacy" are from prior to the submodule system.
+ * They are exported from the package's root index to preserve backwards compatibility.
+ *
+ * New development should go in a proper submodule and not be exported from the root index.
+ */
+
+/**
+ * Legacy submodule list.
+ */
+export * from "./submodules/middleware-http-auth-scheme/index";
+export * from "./submodules/middleware-http-signing/index";
+export * from "./submodules/util-identity-and-auth/index";
+export * from "./submodules/protocols/index";
+export { createPaginator } from "./submodules/pagination/index";
+
 export * from "./getSmithyContext";
 export * from "./normalizeProvider";
-export * from "./protocols/requestBuilder";
-export { createPaginator } from "./pagination/createPaginator";
+
+/**
+ * Warning: do not export any additional submodules from the root of this package. See readme.md for
+ * guide on developing submodules.
+ */
