101fea4f27c6c36638f4408506944dee20a39a69
siddsriv <srivastava41099@gmail.com>
2024-05-20 16:39:55 +0000

    chore: docstring and license update

--- smithy-typescript-codegen/src/main/java/software/amazon/smithy/typescript/codegen/endpointsV2/OmitEndpointParams.java
+++ smithy-typescript-codegen/src/main/java/software/amazon/smithy/typescript/codegen/endpointsV2/OmitEndpointParams.java
@@ -1,26 +1,15 @@
 /*
- * Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License").
- * You may not use this file except in compliance with the License.
- * A copy of the License is located at
- *
- *  http://aws.amazon.com/apache2.0
- *
- * or in the "license" file accompanying this file. This file is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 package software.amazon.smithy.typescript.codegen.endpointsV2;
-
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
 /**
- * Manages a collection of endpoint parameter names to be omitted from a specific interface.
+ * Manages a collection of endpoint parameter names to be omitted from a specific interface. While this could be extensible in the future, as of right now, this collection is maintaining endpoint parameter names to be omitted from the `ClientInputEndpointParameters` interface.
  */
 public final class OmitEndpointParams {
     private static final Set<String> OMITTED_PARAMS = new HashSet<>();
