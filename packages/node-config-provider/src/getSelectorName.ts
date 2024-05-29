d514abb6a1853f5fcc26bc22686f987081d0fd0c
George Fu <kuhe@users.noreply.github.com>
2024-05-29 13:01:01 -0400

    set internal tag on getSelectorName

--- packages/node-config-provider/src/getSelectorName.ts
+++ packages/node-config-provider/src/getSelectorName.ts
@@ -1,7 +1,7 @@
 /**
  * Attempts to extract the name of the variable that the functional selector is looking for.
  * Improves readability over the raw Function.toString() value.
- *
+ * @internal
  * @param functionString - function's string representation.
  *
  * @returns constant value used within the function.
