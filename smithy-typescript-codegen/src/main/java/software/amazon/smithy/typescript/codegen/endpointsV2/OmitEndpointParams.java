e2ff0e7e41fa60dc686668dc4d2ad6353e1302e9
siddsriv <srivastava41099@gmail.com>
2024-06-12 16:46:22 +0000

    chore: minor nit refactor

--- smithy-typescript-codegen/src/main/java/software/amazon/smithy/typescript/codegen/endpointsV2/OmitEndpointParams.java
+++ smithy-typescript-codegen/src/main/java/software/amazon/smithy/typescript/codegen/endpointsV2/OmitEndpointParams.java
@@ -3,13 +3,14 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package software.amazon.smithy.typescript.codegen.endpointsV2;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
+package software.amazon.smithy.typescript.codegen.endpointsV2;
 
 /**
- * Manages a collection of endpoint parameter names to be omitted from a specific interface. While this could be extensible in the future, as of right now, this collection is maintaining endpoint parameter names to be omitted from the `ClientInputEndpointParameters` interface.
+ * Manages a collection of endpoint parameter names to be omitted from a specific interface. 
+ * While this could be extensible in the future, as of right now, this collection is maintaining endpoint parameter names to be omitted from the `ClientInputEndpointParameters` interface.
  */
 public final class OmitEndpointParams {
     private static final Set<String> OMITTED_PARAMS = new HashSet<>();
