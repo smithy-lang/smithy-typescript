7ce73994bc185bb7befa6406dead3dcf6d277f8c
siddsriv <srivastava41099@gmail.com>
2024-06-12 16:46:22 +0000

    chore: minor nit refactor

--- smithy-typescript-codegen/src/main/java/software/amazon/smithy/typescript/codegen/endpointsV2/OmitEndpointParams.java
+++ smithy-typescript-codegen/src/main/java/software/amazon/smithy/typescript/codegen/endpointsV2/OmitEndpointParams.java
@@ -12,8 +12,8 @@ package software.amazon.smithy.typescript.codegen.endpointsV2;
  * Manages a collection of endpoint parameter names to be omitted from a specific interface. 
  * While this could be extensible in the future, as of right now, this collection is maintaining endpoint parameter names to be omitted from the `ClientInputEndpointParameters` interface.
  */
-public final class OmitEndpointParams {
-    private static final Set<String> OMITTED_PARAMS = new HashSet<>();
+public class OmitEndpointParams {
+    private static Set<String> OMITTED_PARAMS = new HashSet<>();
 
     private OmitEndpointParams() {}
     
