3c23a83b8ee11ff92922411259bc0bec9a6ec935
George Fu <kuhe@users.noreply.github.com>
2024-06-13 16:33:38 -0400

    chore: update @aws-crypto/* to 5.2.0 series (#1306)
    * chore: update @aws-crypto/* to 5.2.0 series
    
    * fix unit test
    
    * update yarn lock

--- packages/hash-blob-browser/src/index.spec.ts
+++ packages/hash-blob-browser/src/index.spec.ts
@@ -1,15 +1,24 @@
-import { Sha256 } from "@aws-crypto/sha256-js";
 import { toHex } from "@smithy/util-hex-encoding";
 
 import { blobHasher } from "./index";
 
 describe("blobHasher", () => {
-  const blob = new Blob(["Shot through the bar, but you're too late bizzbuzz you give foo, a bad name."]);
+  const blob = new Blob(["test-string"]);
 
-  it("calculates the SHA256 hash of a blob", async () => {
-    const result = await blobHasher(Sha256, blob);
+  class Hash {
+    public value: string;
+    update(value: string) {
+      this.value = value;
+    }
+    async digest() {
+      return new TextEncoder().encode(this.value);
+    }
+  }
+
+  it("calls update and digest of the given Hash class on the blob", async () => {
+    const result = await blobHasher(Hash, blob);
 
     expect(result instanceof Uint8Array).toBe(true);
-    expect(toHex(result)).toBe("24dabf4db3774a3224d571d4c089a9c570c3045dbe1e67ee9ee2e2677f57dbe0");
+    expect(toHex(result)).toBe("3131362c3130312c3131352c3131362c34352c3131352c3131362c3131342c3130352c3131302c313033");
   });
 });
