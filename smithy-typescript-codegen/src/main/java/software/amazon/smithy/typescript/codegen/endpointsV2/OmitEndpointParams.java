d8eed53aebfdf6f30475ff80ebb601f95a07f3c8
sid <48153483+siddsriv@users.noreply.github.com>
2024-06-21 13:27:15 -0400

    chore: rm blankspace

--- smithy-typescript-codegen/src/main/java/software/amazon/smithy/typescript/codegen/endpointsV2/OmitEndpointParams.java
+++ smithy-typescript-codegen/src/main/java/software/amazon/smithy/typescript/codegen/endpointsV2/OmitEndpointParams.java
@@ -8,12 +8,12 @@
  import java.util.Collections;
  import java.util.HashSet;
  import java.util.Set;
+
  /**
   * Manages a collection of endpoint parameter names to be omitted from a specific interface.
   * While this could be extensible in the future, as of right now,
   * this collection is maintaining endpoint params to be omitted from the `ClientInputEndpointParameters` interface.
   */
-
  public final class OmitEndpointParams {
      private static final Set<String> OMITTED_PARAMS = new HashSet<>();
 
