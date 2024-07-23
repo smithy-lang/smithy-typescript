d3be3fc3b26a1e14a6e4f2c3b21ab5b08b6f94a0
Andre Araujo <arauan@amazon.com>
2024-07-23 11:10:17 +0100

    fix(maxAttempts): Add new configuration maxAttempts in RemoteProviderConfig
    The configuration maxRetries was changed to maxAttempts in middleware-retry to be compliant with other SDKs and retry strategy #1244. This change adds new configuration maxAttempts in RemoteProviderConfig and deprecates maxRetries.

--- packages/credential-provider-imds/src/remoteProvider/RemoteProviderInit.spec.ts
+++ packages/credential-provider-imds/src/remoteProvider/RemoteProviderInit.spec.ts
@@ -17,4 +17,48 @@ describe("providerConfigFromInit", () => {
       maxRetries,
     });
   });
+
+  it("should return maxAttempts with the same value as the deprecated maxRetries if maxAttempts is NOT provided", () => {
+    const timeout = 123456789;
+    const maxRetries = 987654321;
+
+    expect(providerConfigFromInit({ timeout, maxRetries })).toEqual({
+      timeout,
+      maxRetries,
+      maxAttempts: maxRetries,
+    });
+  });
+
+  it("should return maxAttempts with the same value as maxAttempts option if it is provided", () => {
+    const timeout = 123456789;
+    const maxRetries = 987654321;
+    const maxAttempts = 987654322;
+
+    expect(providerConfigFromInit({ timeout, maxRetries, maxAttempts })).toEqual({
+      timeout,
+      maxRetries,
+      maxAttempts,
+    });
+  });
+
+  it("should return maxAttempts with DEFAULT_MAX_RETRIES as value if both maxAttempts and maxRetries are NOT provided", () => {
+    const timeout = 123456789;
+
+    expect(providerConfigFromInit({ timeout })).toEqual({
+      timeout,
+      maxRetries: 0,
+      maxAttempts: 0,
+    });
+  });
+
+  it("should return maxRetries with the same value as the new maxRetries maxAttempts if maxAttempts is provided with any value", () => {
+    const timeout = 123456789;
+    const maxAttempts = 987654321;
+
+    expect(providerConfigFromInit({ timeout, maxAttempts })).toEqual({
+      timeout,
+      maxRetries: maxAttempts,
+      maxAttempts,
+    });
+  });
 });
