ce9edcfef54b5b3186bfc906d1cc393c8f552e2e
George Fu <kuhe@users.noreply.github.com>
2024-08-14 20:07:42 +0000

    feat(cbor): add doc block to submodule enum

--- smithy-typescript-codegen/src/main/java/software/amazon/smithy/typescript/codegen/SmithyCoreSubmodules.java
+++ smithy-typescript-codegen/src/main/java/software/amazon/smithy/typescript/codegen/SmithyCoreSubmodules.java
@@ -5,6 +5,21 @@
 
 package software.amazon.smithy.typescript.codegen;
 
+/**
+ * This is an enum-like submodule list to be used with
+ *
+ * TypeScriptWriter::addImportSubmodule(
+ *   Dependency == "cbor",
+ *   null (no alias),
+ *   PackageContainer == "@smithy/core",
+ *   SmithyCoreSubmodules.CBOR == "/cbor"
+ * );
+ *
+ * The intended result is e.g.
+ * ```ts
+ * import { cbor } from "@smithy/core/cbor";
+ * ```
+ */
 public final class SmithyCoreSubmodules {
     public static final String CBOR = "/cbor";
 
