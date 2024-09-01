b7ead129f59e835cac3f9e2979304ff57ca0d2af
George Fu <kuhe@users.noreply.github.com>
2024-09-01 16:03:04 +0000

    feat(node-http-handler): defer socket event listener attachment

--- packages/node-http-handler/src/set-connection-timeout.ts
+++ packages/node-http-handler/src/set-connection-timeout.ts
@@ -1,28 +1,43 @@
 import { ClientRequest } from "http";
 import { Socket } from "net";
 
-export const setConnectionTimeout = (request: ClientRequest, reject: (err: Error) => void, timeoutInMs = 0) => {
+const DEFER_EVENT_LISTENER_TIME = 1000;
+
+export const setConnectionTimeout = (
+  request: ClientRequest,
+  reject: (err: Error) => void,
+  timeoutInMs = 0
+): NodeJS.Timeout | number => {
   if (!timeoutInMs) {
-    return;
+    return -1;
   }
 
-  // Throw a connecting timeout error unless a connection is made within time.
-  const timeoutId = setTimeout(() => {
-    request.destroy();
-    reject(
-      Object.assign(new Error(`Socket timed out without establishing a connection within ${timeoutInMs} ms`), {
-        name: "TimeoutError",
-      })
-    );
-  }, timeoutInMs);
+  const registerTimeout = (offset: number) => {
+    // Throw a connecting timeout error unless a connection is made within time.
+    const timeoutId = setTimeout(() => {
+      request.destroy();
+      reject(
+        Object.assign(new Error(`Socket timed out without establishing a connection within ${timeoutInMs} ms`), {
+          name: "TimeoutError",
+        })
+      );
+    }, timeoutInMs - offset);
 
-  request.on("socket", (socket: Socket) => {
-    if (socket.connecting) {
-      socket.on("connect", () => {
+    request.on("socket", (socket: Socket) => {
+      if (socket.connecting) {
+        socket.on("connect", () => {
+          clearTimeout(timeoutId);
+        });
+      } else {
         clearTimeout(timeoutId);
-      });
-    } else {
-      clearTimeout(timeoutId);
-    }
-  });
+      }
+    });
+  };
+
+  if (timeoutInMs < 2000) {
+    registerTimeout(0);
+    return 0;
+  }
+
+  return setTimeout(registerTimeout.bind(null, DEFER_EVENT_LISTENER_TIME), DEFER_EVENT_LISTENER_TIME);
 };
