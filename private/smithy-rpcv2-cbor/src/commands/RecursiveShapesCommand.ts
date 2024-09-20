91158accac297e7b05f626d060d4832e4edc187a
George Fu <kuhe@users.noreply.github.com>
2024-09-20 15:47:32 +0000

    chore: update protocol test codegen output

--- private/smithy-rpcv2-cbor/src/commands/RecursiveShapesCommand.ts
+++ private/smithy-rpcv2-cbor/src/commands/RecursiveShapesCommand.ts
@@ -94,4 +94,16 @@ export class RecursiveShapesCommand extends $Command
   .f(void 0, void 0)
   .ser(se_RecursiveShapesCommand)
   .de(de_RecursiveShapesCommand)
-  .build() {}
+  .build() {
+  /** @internal type navigation helper, not in runtime. */
+  protected declare static __types: {
+    api: {
+      input: RecursiveShapesInputOutput;
+      output: RecursiveShapesInputOutput;
+    };
+    sdk: {
+      input: RecursiveShapesCommandInput;
+      output: RecursiveShapesCommandOutput;
+    };
+  };
+}
