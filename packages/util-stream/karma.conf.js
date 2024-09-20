483742d48f16e04d8d010eafaddcdefde9f16242
George Fu <kuhe@users.noreply.github.com>
2024-09-20 18:14:56 +0000

    add karma test for checksum stream

--- packages/util-stream/karma.conf.js
+++ packages/util-stream/karma.conf.js
@@ -3,7 +3,14 @@
 module.exports = function (config) {
   config.set({
     frameworks: ["jasmine", "karma-typescript"],
-    files: ["src/getAwsChunkedEncodingStream.browser.ts", "src/getAwsChunkedEncodingStream.browser.spec.ts"],
+    files: [
+      "src/checksum/createChecksumStream.browser.spec.ts",
+      "src/checksum/createChecksumStream.browser.ts",
+      "src/getAwsChunkedEncodingStream.browser.spec.ts",
+      "src/getAwsChunkedEncodingStream.browser.ts",
+      "src/headStream.browser.ts",
+      "src/stream-type-check.ts",
+    ],
     exclude: ["**/*.d.ts"],
     preprocessors: {
       "**/*.ts": "karma-typescript",
