295a8edbed741f5009c62c9f5d730cda5e37a82c
George Fu <kuhe@users.noreply.github.com>
2024-10-04 19:49:46 +0000

    add test assertion

--- packages/core/src/setFeature.spec.ts
+++ packages/core/src/setFeature.spec.ts
@@ -6,5 +6,12 @@ describe(setFeature.name, () => {
   it("creates the context object path if needed", () => {
     const context: HandlerExecutionContext = {};
     setFeature(context, "RETRY_MODE_STANDARD", "E");
+    expect(context).toEqual({
+      __smithy_context: {
+        features: {
+          RETRY_MODE_STANDARD: "E",
+        },
+      },
+    });
   });
 });
