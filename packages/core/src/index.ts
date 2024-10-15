d07b0aba6f02b47d391001327d95ee9a3b4bbf2e
George Fu <kuhe@users.noreply.github.com>
2024-10-15 12:34:33 -0400

    chore: feature detection for gzip and endpoint override (#1429)
    * chore: feature detection for gzip and endpoint override
    
    * reorganize core upstream of smithy-client
    
    * update lockfile
    
    * reorganize core and smithy-client

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,7 +1,8 @@
+export * from "./getSmithyContext";
 export * from "./middleware-http-auth-scheme";
 export * from "./middleware-http-signing";
-export * from "./util-identity-and-auth";
-export * from "./getSmithyContext";
 export * from "./normalizeProvider";
-export * from "./protocols/requestBuilder";
 export { createPaginator } from "./pagination/createPaginator";
+export * from "./protocols/requestBuilder";
+export * from "./setFeature";
+export * from "./util-identity-and-auth";
