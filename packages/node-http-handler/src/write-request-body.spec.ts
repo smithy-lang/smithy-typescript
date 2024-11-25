8be46100cb3e3b6445e2dec38831dea5d74a60f6
George Fu <kuhe@users.noreply.github.com>
2024-11-25 16:31:53 +0000

    add unit tests

---
+++ packages/node-http-handler/src/write-request-body.spec.ts
@@ -0,0 +1,62 @@
+import EventEmitter from "events";
+import { describe, expect, test as it, vi } from "vitest";
+
+import { writeRequestBody } from "./write-request-body";
+
+describe(writeRequestBody.name, () => {
+  it("should wait for the continue event if request has expect=100-continue", async () => {
+    const httpRequest = Object.assign(new EventEmitter(), {
+      end: vi.fn(),
+    }) as any;
+    const request = {
+      headers: {
+        expect: "100-continue",
+      },
+      body: Buffer.from("abcd"),
+      method: "GET",
+      hostname: "",
+      protocol: "https:",
+      path: "/",
+    };
+    let done: (value?: unknown) => void;
+    const promise = new Promise((r) => (done = r));
+    setTimeout(async () => {
+      httpRequest.emit("continue", {});
+      done();
+    }, 25);
+    await writeRequestBody(httpRequest, request);
+    expect(httpRequest.end).toHaveBeenCalled();
+    await promise;
+  });
+  it(
+    "should not send the body if the request is expect=100-continue" +
+      "but a response is received before the continue event",
+    async () => {
+      const httpRequest = Object.assign(new EventEmitter(), {
+        end: vi.fn(),
+      }) as any;
+      const request = {
+        headers: {
+          expect: "100-continue",
+        },
+        body: {
+          pipe: vi.fn(),
+        },
+        method: "GET",
+        hostname: "",
+        protocol: "https:",
+        path: "/",
+      };
+      let done: (value?: unknown) => void;
+      const promise = new Promise((r) => (done = r));
+      setTimeout(() => {
+        httpRequest.emit("response", {});
+        done();
+      }, 25);
+      await writeRequestBody(httpRequest, request);
+      expect(request.body.pipe).not.toHaveBeenCalled();
+      expect(httpRequest.end).not.toHaveBeenCalled();
+      await promise;
+    }
+  );
+});
