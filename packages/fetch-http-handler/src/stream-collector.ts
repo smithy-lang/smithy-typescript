fed71a350c44a37de4064b29627d29b153c609ef
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2025-01-01 21:30:00 -0800

    fix(fetch-http-handler): add polyfill to collect Blob

--- packages/fetch-http-handler/src/stream-collector.ts
+++ packages/fetch-http-handler/src/stream-collector.ts
@@ -1,12 +1,22 @@
 import { StreamCollector } from "@smithy/types";
+import { fromBase64 } from "@smithy/util-base64";
 
 export const streamCollector: StreamCollector = async (stream: Blob | ReadableStream): Promise<Uint8Array> => {
   if ((typeof Blob === "function" && stream instanceof Blob) || stream.constructor?.name === "Blob") {
-    return new Uint8Array(await (stream as Blob).arrayBuffer());
+    if (Blob.prototype.arrayBuffer !== undefined) {
+      return new Uint8Array(await (stream as Blob).arrayBuffer());
+    }
+    return collectBlob(stream as Blob);
   }
 
   return collectStream(stream as ReadableStream);
 };
+
+async function collectBlob(blob: Blob): Promise<Uint8Array> {
+  const base64 = await readToBase64(blob);
+  const arrayBuffer = fromBase64(base64);
+  return new Uint8Array(arrayBuffer);
+}
 
 async function collectStream(stream: ReadableStream): Promise<Uint8Array> {
   const chunks = [];
@@ -31,4 +41,27 @@ async function collectStream(stream: ReadableStream): Promise<Uint8Array> {
   }
 
   return collected;
+}
+
+function readToBase64(blob: Blob): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      // reference: https://developer.mozilla.org/en-US/docs/Web/API/FileReader/readAsDataURL
+      // response from readAsDataURL is always prepended with "data:*/*;base64,"
+      if (reader.readyState !== 2) {
+        return reject(new Error("Reader aborted too early"));
+      }
+      const result = (reader.result ?? "") as string;
+      // Response can include only 'data:' for empty blob, return empty string in this case.
+      // Otherwise, return the string after ','
+      const commaIndex = result.indexOf(",");
+      const dataOffset = commaIndex > -1 ? commaIndex + 1 : result.length;
+      resolve(result.substring(dataOffset));
+    };
+    reader.onabort = () => reject(new Error("Read aborted"));
+    reader.onerror = () => reject(reader.error);
+    // reader.readAsArrayBuffer is not always available
+    reader.readAsDataURL(blob);
+  });
 }
