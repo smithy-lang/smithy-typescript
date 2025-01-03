5dec77e58d69a3168d2bec0ddb3baf16262ce91c
siddsriv <srivastava41099@gmail.com>
2025-01-03 21:12:40 +0000

    chore(deps-dev): bump @types/node

--- smithy-typescript-codegen/src/main/java/software/amazon/smithy/typescript/codegen/TypeScriptDependency.java
+++ smithy-typescript-codegen/src/main/java/software/amazon/smithy/typescript/codegen/TypeScriptDependency.java
@@ -45,7 +45,7 @@ public enum TypeScriptDependency implements Dependency {
     AWS_SMITHY_CLIENT("dependencies", "@smithy/smithy-client", true),
     INVALID_DEPENDENCY("dependencies", "@smithy/invalid-dependency", true),
     CONFIG_RESOLVER("dependencies", "@smithy/config-resolver", true),
-    TYPES_NODE("devDependencies", "@types/node", "^18.11.9", true),
+    TYPES_NODE("devDependencies", "@types/node", "^18.19.69", true),
 
     MIDDLEWARE_CONTENT_LENGTH("dependencies", "@smithy/middleware-content-length", true),
     MIDDLEWARE_SERDE("dependencies", "@smithy/middleware-serde", true),
