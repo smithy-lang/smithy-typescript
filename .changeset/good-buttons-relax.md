fbd06ebd631ce8a1a642380abb499dcbb7ea1329
Tommy Brunn <tommy.brunn@gmail.com>
2025-01-15 20:43:27 +0100

    fix(node-http-handler): Write request body if 100 Continue response takes more than 1 second (#1505)
    * fix(node-http-handler): Write request body if 100 Continue response times out
    
    * set 100-continue timeout to 6s minimum
    
    ---------
    
    Co-authored-by: George Fu <kuhe@users.noreply.github.com>

---
+++ .changeset/good-buttons-relax.md
@@ -0,0 +1,5 @@
+---
+"@smithy/node-http-handler": patch
+---
+
+fix sending request when 100 Continue response takes more than 1 second
--- packages/node-http-handler/src/write-request-body.spec.ts
+++ packages/node-http-handler/src/write-request-body.spec.ts
@@ -1,5 +1,5 @@
 import EventEmitter from "events";
-import { describe, expect, test as it, vi } from "vitest";
+import { afterEach, beforeEach, describe, expect, test as it, vi } from "vitest";
 
 import { writeRequestBody } from "./write-request-body";
 
@@ -59,4 +59,36 @@ describe(writeRequestBody.name, () => {
       await promise;
     }
   );
+
+  describe("with fake timers", () => {
+    beforeEach(() => {
+      vi.useFakeTimers();
+    });
+
+    afterEach(() => {
+      vi.useRealTimers();
+    });
+    it("should send the body if the 100 Continue response is not received before the timeout", async () => {
+      const httpRequest = Object.assign(new EventEmitter(), {
+        end: vi.fn(),
+      }) as any;
+      const request = {
+        headers: {
+          expect: "100-continue",
+        },
+        body: Buffer.from("abcd"),
+        method: "GET",
+        hostname: "",
+        protocol: "https:",
+        path: "/",
+      };
+
+      const promise = writeRequestBody(httpRequest, request, 12_000);
+      expect(httpRequest.end).not.toHaveBeenCalled();
+      vi.advanceTimersByTime(13000);
+      await promise;
+
+      expect(httpRequest.end).toHaveBeenCalled();
+    });
+  });
 });
