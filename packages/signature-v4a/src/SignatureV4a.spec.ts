2864e36d69b723bb55e39d311fe6839b9d37140d
siddsriv <srivastava41099@gmail.com>
2025-04-01 02:55:40 +0000

    test(signature-v4a): move testing from jest to vitest

--- packages/signature-v4a/src/SignatureV4a.spec.ts
+++ packages/signature-v4a/src/SignatureV4a.spec.ts
@@ -1,5 +1,6 @@
 import { Sha256 } from "@aws-crypto/sha256-js";
 import { AwsCredentialIdentity, HttpRequest } from "@smithy/types";
+import { describe, it, expect } from 'vitest';
 
 import { SignatureV4a } from "./SignatureV4a";
 
