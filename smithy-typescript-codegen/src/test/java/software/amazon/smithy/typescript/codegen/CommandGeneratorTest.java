2b9183ea280d159c99fbc3012f05bed1a058f25d
George Fu <kuhe@users.noreply.github.com>
2025-04-09 12:26:33 -0400

    fix(codegen): fix jmespath flatten level

--- smithy-typescript-codegen/src/test/java/software/amazon/smithy/typescript/codegen/CommandGeneratorTest.java
+++ smithy-typescript-codegen/src/test/java/software/amazon/smithy/typescript/codegen/CommandGeneratorTest.java
@@ -12,7 +12,7 @@ import software.amazon.smithy.model.node.Node;
 public class CommandGeneratorTest {
     @Test
     public void addsCommandSpecificPlugins() {
-        testCommmandCodegen(
+        testCommandCodegen(
             "output-structure.smithy",
             new String[] {"getSerdePlugin(config, this.serialize, this.deserialize)"}
         );
@@ -20,7 +20,7 @@ public class CommandGeneratorTest {
 
     @Test
     public void writesSerializer() {
-        testCommmandCodegen(
+        testCommandCodegen(
             "output-structure.smithy",
             new String[] {".ser("}
         );
@@ -28,7 +28,7 @@ public class CommandGeneratorTest {
 
     @Test
     public void writesDeserializer() {
-        testCommmandCodegen(
+        testCommandCodegen(
             "output-structure.smithy",
             new String[] {".de("}
         );
@@ -36,24 +36,34 @@ public class CommandGeneratorTest {
 
     @Test
     public void writesOperationContextParamValues() {
-        testCommmandCodegen(
+        testCommandCodegen(
             "endpointsV2/endpoints-operation-context-params.smithy",
             new String[] {
-                "opContextParamIdentifier: { type: \"operationContextParams\", get: (input?: any) => input?.fooString }",
-                "opContextParamSubExpression: { type: \"operationContextParams\", get: (input?: any) => input?.fooObj?.bar }",
-                "opContextParamWildcardExpressionList: { type: \"operationContextParams\", get: (input?: any) => input?.fooList }",
-                "opContextParamWildcardExpressionListFlatten: { type: \"operationContextParams\", get: (input?: any) => input?.fooListList.flat() }",
-                "opContextParamWildcardExpressionListObj: { type: \"operationContextParams\", get: (input?: any) => input?.fooListObj?.map((obj: any) => obj?.key) }",
-                "opContextParamWildcardExpressionListObjListFlatten: { type: \"operationContextParams\", get: (input?: any) => input?.fooListObjList?.map((obj: any) => obj?.key).flat() }",
-                "opContextParamWildcardExpressionHash: { type: \"operationContextParams\", get: (input?: any) => Object.values(input?.fooObjObj ?? {}).map((obj: any) => obj?.bar) }",
-                "opContextParamMultiSelectList: { type: \"operationContextParams\", get: (input?: any) => input?.fooListObjObj?.map((obj: any) => [obj?.fooObject?.bar,obj?.fooString].filter((i) => i)) }",
-                "opContextParamMultiSelectListFlatten: { type: \"operationContextParams\", get: (input?: any) => input?.fooListObjObj?.map((obj: any) => [obj?.fooList].filter((i) => i).flat()) }",
-                "opContextParamKeys: { type: \"operationContextParams\", get: (input?: any) => Object.keys(input?.fooKeys ?? {}) }",
+                """
+opContextParamIdentifier: { type: "operationContextParams", get: (input?: any) => input?.fooString }""",
+                """
+opContextParamSubExpression: { type: "operationContextParams", get: (input?: any) => input?.fooObj?.bar }""",
+                """
+opContextParamWildcardExpressionList: { type: "operationContextParams", get: (input?: any) => input?.fooList }""",
+                """
+opContextParamWildcardExpressionListFlatten: { type: "operationContextParams", get: (input?: any) => input?.fooListList.flat() }""",
+                """
+opContextParamWildcardExpressionListObj: { type: "operationContextParams", get: (input?: any) => input?.fooListObj?.map((obj: any) => obj?.key) }""",
+                """
+opContextParamWildcardExpressionListObjListFlatten: { type: "operationContextParams", get: (input?: any) => input?.fooListObjList?.map((obj: any) => obj?.key).flat() }""",
+                """
+opContextParamWildcardExpressionHash: { type: "operationContextParams", get: (input?: any) => Object.values(input?.fooObjObj ?? {}).map((obj: any) => obj?.bar) }""",
+                """
+opContextParamMultiSelectList: { type: "operationContextParams", get: (input?: any) => input?.fooListObjObj?.map((obj: any) => [obj?.fooObject?.bar,obj?.fooString].filter((i) => i)) }""",
+                """
+opContextParamMultiSelectListFlatten: { type: "operationContextParams", get: (input?: any) => input?.fooListObjObj?.map((obj: any) => [obj?.fooList].filter((i) => i)).flat() }""",
+                """
+opContextParamKeys: { type: "operationContextParams", get: (input?: any) => Object.keys(input?.fooKeys ?? {}) }""",
             }
         );
     }
 
-    private void testCommmandCodegen(String filename, String[] expectedTypeArray) {
+    private void testCommandCodegen(String filename, String[] expectedTypeArray) {
         MockManifest manifest = new MockManifest();
         PluginContext context = PluginContext.builder()
             .pluginClassLoader(getClass().getClassLoader())
