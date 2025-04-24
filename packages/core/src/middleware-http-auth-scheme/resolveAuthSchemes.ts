07bcfce562146c65f9b3ac76e2e8ed693b14afb8
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2025-04-24 21:14:30 +0000

    chore(core): add utility resolveAuthSchemes

---
+++ packages/core/src/middleware-http-auth-scheme/resolveAuthSchemes.ts
@@ -0,0 +1,34 @@
+import { HttpAuthScheme } from "@smithy/types";
+
+/**
+ * Resolves list of auth schemes based on the supported ones, vs the preference list.
+ * 
+ * @param candidateAuthSchemes list of supported auth schemes selected by the standard
+ *   resolution process (model-based, endpoints 2.0, etc.)
+ * @param authSchemePreference list of auth schemes preferred by user.
+ * @returns 
+ */
+export const resolveAuthSchemes = (candidateAuthSchemes: HttpAuthScheme[], authSchemePreference: string[]) => {
+    if (!authSchemePreference || authSchemePreference.length === 0) {
+      // reprioritize candidates based on user's preference
+      const preferredAuthSchemes = [];
+      
+      for (const preferredSchemeName of authSchemePreference) {
+        for (const candidateAuthScheme of candidateAuthSchemes) {
+          const candidateAuthSchemeName = candidateAuthScheme.schemeId.split("#")[1]
+          if (candidateAuthSchemeName === preferredSchemeName) {
+            preferredAuthSchemes.push(candidateAuthScheme);
+          }
+        }
+      }
+      
+      // add any remaining candidates that weren't in the preference list
+      for (const candidateAuthScheme of candidateAuthSchemes) {
+        if (!preferredAuthSchemes.find(({ schemeId }) => schemeId === candidateAuthScheme.schemeId)) {
+          preferredAuthSchemes.push(candidateAuthScheme);
+        }
+      }
+    } else {
+      return candidateAuthSchemes;
+    }
+  }
