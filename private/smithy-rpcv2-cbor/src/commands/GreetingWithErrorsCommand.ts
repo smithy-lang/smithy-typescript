97e6d1ca0327e161b57603b68b377140a26a4f82
George Fu <kuhe@users.noreply.github.com>
2025-05-05 12:24:21 -0400

    chore: update codegen for protocol tests

--- private/smithy-rpcv2-cbor/src/commands/GreetingWithErrorsCommand.ts
+++ private/smithy-rpcv2-cbor/src/commands/GreetingWithErrorsCommand.ts
@@ -63,6 +63,7 @@ export interface GreetingWithErrorsCommandOutput extends GreetingWithErrorsOutpu
  * @throws {@link RpcV2ProtocolServiceException}
  * <p>Base exception class for all service exceptions from RpcV2Protocol service.</p>
  *
+ *
  * @public
  */
 export class GreetingWithErrorsCommand extends $Command
