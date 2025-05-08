1a84065f6884375d749e1a69dfbbb0b5028d544b
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2025-05-08 16:28:00 +0000

    chore(node-config-provider): export Getters and their configs

--- packages/node-config-provider/src/index.ts
+++ packages/node-config-provider/src/index.ts
@@ -1,1 +1,4 @@
 export * from "./configLoader";
+export { EnvOptions, GetterFromEnv } from "./fromEnv";
+export { GetterFromConfig, SharedConfigInit } from "./fromSharedConfigFiles";
+export { FromStaticConfig } from "./fromStatic";
