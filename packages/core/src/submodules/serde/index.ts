06b0ce8c21178b3e8f78d4008bcf79fe0c149a76
George Fu <kuhe@users.noreply.github.com>
2025-05-14 17:08:38 -0400

    chore(schema): move serde functions from smithy-client to core/serde (#1594)

--- packages/core/src/submodules/serde/index.ts
+++ packages/core/src/submodules/serde/index.ts
@@ -1,1 +1,7 @@
+export * from "./parse-utils";
+export * from "./date-utils";
+export * from "./quote-header";
+export * from "./split-header";
 export * from "./value/NumericValue";
+export * from "./lazy-json";
+export * from "./split-every";
