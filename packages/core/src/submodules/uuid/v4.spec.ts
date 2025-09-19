3fc3a3c2ead1cb28d0494c75652ab0c27c256bce
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2025-09-19 17:27:03 +0000

    chore: rename uuid.randomUUID to v4

--- packages/core/src/submodules/uuid/v4.spec.ts
+++ packages/core/src/submodules/uuid/v4.spec.ts
@@ -11,20 +11,20 @@ describe("randomUUID", () => {
     const nativeRandomUUID = vi.fn(() => mockUUID);
     vi.doMock("./randomUUID", () => ({ randomUUID: nativeRandomUUID }));
 
-    const { randomUUID } = await import("./index");
-    const uuid = randomUUID();
+    const { v4 } = await import("./v4");
+    const uuid = v4();
 
     expect(nativeRandomUUID).toHaveBeenCalled();
     expect(uuid).toBe(mockUUID);
   });
 
   describe("when native randomUUID is not available", () => {
-    let randomUUID: any;
+    let v4: any;
     const UUID_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
     beforeEach(async () => {
       vi.doMock("./randomUUID", () => ({ randomUUID: undefined }));
-      randomUUID = (await import("./index")).randomUUID;
+      v4 = (await import("./v4")).v4;
 
       // Simulate crypto.getRandomValues in test, as it's expected to be available
       global.crypto = {
@@ -36,7 +36,7 @@ describe("randomUUID", () => {
       const uuids = new Set();
       const iterations = 10_000;
       for (let i = 0; i < iterations; i++) {
-        const uuid = randomUUID();
+        const uuid = v4();
         expect(uuid).toMatch(UUID_REGEX);
         uuids.add(uuid);
       }
