84a0075fa9a3e4e6f4cac5b7552d47623450b564
George Fu <kuhe@users.noreply.github.com>
2025-10-03 16:03:51 -0400

    fix(codegen): cbor eventstream codegen (#1731)

---
+++ private/my-local-model/src/commands/TradeEventStreamCommand.ts
@@ -0,0 +1,121 @@
+// smithy-typescript generated code
+import { ServiceInputTypes, ServiceOutputTypes, XYZServiceClientResolvedConfig } from "../XYZServiceClient";
+import { commonParams } from "../endpoint/EndpointParameters";
+import {
+  TradeEventStreamRequest,
+  TradeEventStreamRequestFilterSensitiveLog,
+  TradeEventStreamResponse,
+  TradeEventStreamResponseFilterSensitiveLog,
+} from "../models/models_0";
+import { de_TradeEventStreamCommand, se_TradeEventStreamCommand } from "../protocols/Rpcv2cbor";
+import { getEndpointPlugin } from "@smithy/middleware-endpoint";
+import { getSerdePlugin } from "@smithy/middleware-serde";
+import { Command as $Command } from "@smithy/smithy-client";
+import { MetadataBearer as __MetadataBearer } from "@smithy/types";
+
+/**
+ * @public
+ */
+export type { __MetadataBearer };
+export { $Command };
+/**
+ * @public
+ *
+ * The input for {@link TradeEventStreamCommand}.
+ */
+export interface TradeEventStreamCommandInput extends TradeEventStreamRequest {}
+/**
+ * @public
+ *
+ * The output of {@link TradeEventStreamCommand}.
+ */
+export interface TradeEventStreamCommandOutput extends TradeEventStreamResponse, __MetadataBearer {}
+
+/**
+ * @public
+ *
+ * @example
+ * Use a bare-bones client and the command you need to make an API call.
+ * ```javascript
+ * import { XYZServiceClient, TradeEventStreamCommand } from "xyz"; // ES Modules import
+ * // const { XYZServiceClient, TradeEventStreamCommand } = require("xyz"); // CommonJS import
+ * // import type { XYZServiceClientConfig } from "xyz";
+ * const config = {}; // type is XYZServiceClientConfig
+ * const client = new XYZServiceClient(config);
+ * const input = { // TradeEventStreamRequest
+ *   eventStream: { // TradeEvents Union: only one key present
+ *     alpha: { // Alpha
+ *       id: "STRING_VALUE",
+ *       timestamp: new Date("TIMESTAMP"),
+ *     },
+ *     beta: {},
+ *     gamma: {},
+ *   },
+ * };
+ * const command = new TradeEventStreamCommand(input);
+ * const response = await client.send(command);
+ * // { // TradeEventStreamResponse
+ * //   eventStream: { // TradeEvents Union: only one key present
+ * //     alpha: { // Alpha
+ * //       id: "STRING_VALUE",
+ * //       timestamp: new Date("TIMESTAMP"),
+ * //     },
+ * //     beta: {},
+ * //     gamma: {},
+ * //   },
+ * // };
+ *
+ * ```
+ *
+ * @param TradeEventStreamCommandInput - {@link TradeEventStreamCommandInput}
+ * @returns {@link TradeEventStreamCommandOutput}
+ * @see {@link TradeEventStreamCommandInput} for command's `input` shape.
+ * @see {@link TradeEventStreamCommandOutput} for command's `response` shape.
+ * @see {@link XYZServiceClientResolvedConfig | config} for XYZServiceClient's `config` shape.
+ *
+ * @throws {@link XYZServiceServiceException}
+ * <p>Base exception class for all service exceptions from XYZService service.</p>
+ *
+ *
+ */
+export class TradeEventStreamCommand extends $Command
+  .classBuilder<
+    TradeEventStreamCommandInput,
+    TradeEventStreamCommandOutput,
+    XYZServiceClientResolvedConfig,
+    ServiceInputTypes,
+    ServiceOutputTypes
+  >()
+  .ep(commonParams)
+  .m(function (this: any, Command: any, cs: any, config: XYZServiceClientResolvedConfig, o: any) {
+    return [
+      getSerdePlugin(config, this.serialize, this.deserialize),
+      getEndpointPlugin(config, Command.getEndpointParameterInstructions()),
+    ];
+  })
+  .s("XYZService", "TradeEventStream", {
+    /**
+     * @internal
+     */
+    eventStream: {
+      input: true,
+      output: true,
+    },
+  })
+  .n("XYZServiceClient", "TradeEventStreamCommand")
+  .f(TradeEventStreamRequestFilterSensitiveLog, TradeEventStreamResponseFilterSensitiveLog)
+  .ser(se_TradeEventStreamCommand)
+  .de(de_TradeEventStreamCommand)
+  .build() {
+  /** @internal type navigation helper, not in runtime. */
+  protected declare static __types: {
+    api: {
+      input: TradeEventStreamRequest;
+      output: TradeEventStreamResponse;
+    };
+    sdk: {
+      input: TradeEventStreamCommandInput;
+      output: TradeEventStreamCommandOutput;
+    };
+  };
+}
--- private/my-local-model/src/models/models_0.ts
+++ private/my-local-model/src/models/models_0.ts
@@ -6,6 +6,14 @@ import { ExceptionOptionType as __ExceptionOptionType } from "@smithy/smithy-cli
 /**
  * @public
  */
+export interface Alpha {
+  id?: string | undefined;
+  timestamp?: Date | undefined;
+}
+
+/**
+ * @public
+ */
 export class CodedThrottlingError extends __BaseException {
   readonly name: "CodedThrottlingError" = "CodedThrottlingError";
   readonly $fault: "client" = "client";
@@ -101,3 +109,106 @@ export class RetryableError extends __BaseException {
     Object.setPrototypeOf(this, RetryableError.prototype);
   }
 }
+
+/**
+ * @public
+ */
+export interface Unit {}
+
+/**
+ * @public
+ */
+export type TradeEvents =
+  | TradeEvents.AlphaMember
+  | TradeEvents.BetaMember
+  | TradeEvents.GammaMember
+  | TradeEvents.$UnknownMember;
+
+/**
+ * @public
+ */
+export namespace TradeEvents {
+  export interface AlphaMember {
+    alpha: Alpha;
+    beta?: never;
+    gamma?: never;
+    $unknown?: never;
+  }
+
+  export interface BetaMember {
+    alpha?: never;
+    beta: Unit;
+    gamma?: never;
+    $unknown?: never;
+  }
+
+  export interface GammaMember {
+    alpha?: never;
+    beta?: never;
+    gamma: Unit;
+    $unknown?: never;
+  }
+
+  /**
+   * @public
+   */
+  export interface $UnknownMember {
+    alpha?: never;
+    beta?: never;
+    gamma?: never;
+    $unknown: [string, any];
+  }
+
+  export interface Visitor<T> {
+    alpha: (value: Alpha) => T;
+    beta: (value: Unit) => T;
+    gamma: (value: Unit) => T;
+    _: (name: string, value: any) => T;
+  }
+
+  export const visit = <T>(value: TradeEvents, visitor: Visitor<T>): T => {
+    if (value.alpha !== undefined) return visitor.alpha(value.alpha);
+    if (value.beta !== undefined) return visitor.beta(value.beta);
+    if (value.gamma !== undefined) return visitor.gamma(value.gamma);
+    return visitor._(value.$unknown[0], value.$unknown[1]);
+  };
+}
+/**
+ * @internal
+ */
+export const TradeEventsFilterSensitiveLog = (obj: TradeEvents): any => {
+  if (obj.alpha !== undefined) return { alpha: obj.alpha };
+  if (obj.beta !== undefined) return { beta: obj.beta };
+  if (obj.gamma !== undefined) return { gamma: obj.gamma };
+  if (obj.$unknown !== undefined) return { [obj.$unknown[0]]: "UNKNOWN" };
+};
+
+/**
+ * @public
+ */
+export interface TradeEventStreamRequest {
+  eventStream?: AsyncIterable<TradeEvents> | undefined;
+}
+
+/**
+ * @internal
+ */
+export const TradeEventStreamRequestFilterSensitiveLog = (obj: TradeEventStreamRequest): any => ({
+  ...obj,
+  ...(obj.eventStream && { eventStream: "STREAMING_CONTENT" }),
+});
+
+/**
+ * @public
+ */
+export interface TradeEventStreamResponse {
+  eventStream?: AsyncIterable<TradeEvents> | undefined;
+}
+
+/**
+ * @internal
+ */
+export const TradeEventStreamResponseFilterSensitiveLog = (obj: TradeEventStreamResponse): any => ({
+  ...obj,
+  ...(obj.eventStream && { eventStream: "STREAMING_CONTENT" }),
+});
