c0cc0c7690d8410898e352f493815345f55bda31
smilkuri <smilkuri@amazon.com>
2025-11-24 16:05:46 +0000

    fix: nest conflicting endpoint parameters

--- smithy-typescript-codegen/src/test/java/software/amazon/smithy/typescript/codegen/endpointsV2/EndpointsV2GeneratorTest.java
+++ smithy-typescript-codegen/src/test/java/software/amazon/smithy/typescript/codegen/endpointsV2/EndpointsV2GeneratorTest.java
@@ -61,6 +61,10 @@ public class EndpointsV2GeneratorTest {
         assertThat(endpointParameters, containsString(
             """
                 export interface ClientInputEndpointParameters {
+                  clientContextParams: {
+                    region?: string | undefined | Provider<string | undefined>;
+                    stage?: string | undefined | Provider<string | undefined>;
+                  };
                   region?: string | undefined | Provider<string | undefined>;
                   stage?: string | undefined | Provider<string | undefined>;
                   endpoint?:"""));
