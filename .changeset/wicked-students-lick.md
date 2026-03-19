b1f0dbaa7fde234daf12a539676f2191afc29ae3
Sreeja Milkuri <smilkuri@amazon.com>
2026-03-19 13:16:42 -0400

    fix(config-resolver): add new config selectors (#1927)

---
+++ .changeset/wicked-students-lick.md
@@ -0,0 +1,7 @@
+---
+"@smithy/middleware-endpoint": patch
+"@smithy/config-resolver": patch
+---
+
+fix(middleware-endpoint): update type of useDualStackEndpoint/useFipsEndpoint input config
+fix(config-resolver): add alternate values for NODE_USE_DUALSTACK_ENDPOINT_CONFIG_OPTIONS and NODE_USE_FIPS_ENDPOINT_CONFIG_OPTIONS
--- packages/config-resolver/src/endpointsConfig/NodeUseDualstackEndpointConfigOptions.ts
+++ packages/config-resolver/src/endpointsConfig/NodeUseDualstackEndpointConfigOptions.ts
@@ -15,9 +15,21 @@ export const CONFIG_USE_DUALSTACK_ENDPOINT = "use_dualstack_endpoint";
 export const DEFAULT_USE_DUALSTACK_ENDPOINT = false;
 
 /**
+ * Don't delete this, used by older clients.
+ * @deprecated replaced by nodeDualstackConfigSelectors in newer clients.
  * @internal
  */
-export const NODE_USE_DUALSTACK_ENDPOINT_CONFIG_OPTIONS: LoadedConfigSelectors<boolean | undefined> = {
+export const NODE_USE_DUALSTACK_ENDPOINT_CONFIG_OPTIONS: LoadedConfigSelectors<boolean> = {
+  environmentVariableSelector: (env: NodeJS.ProcessEnv) =>
+    booleanSelector(env, ENV_USE_DUALSTACK_ENDPOINT, SelectorType.ENV),
+  configFileSelector: (profile) => booleanSelector(profile, CONFIG_USE_DUALSTACK_ENDPOINT, SelectorType.CONFIG),
+  default: false,
+};
+
+/**
+ * @internal
+ */
+export const nodeDualstackConfigSelectors: LoadedConfigSelectors<boolean | undefined> = {
   environmentVariableSelector: (env: NodeJS.ProcessEnv) =>
     booleanSelector(env, ENV_USE_DUALSTACK_ENDPOINT, SelectorType.ENV),
   configFileSelector: (profile) => booleanSelector(profile, CONFIG_USE_DUALSTACK_ENDPOINT, SelectorType.CONFIG),
