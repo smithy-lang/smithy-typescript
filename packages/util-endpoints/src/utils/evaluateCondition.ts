f86da3852f143ee184b0b35a3a40bc574b20eb0a
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2026-04-14 14:41:21 -0700

    perf(util-endpoints): reduce allocations in evaluateCondition

--- packages/util-endpoints/src/utils/evaluateCondition.ts
+++ packages/util-endpoints/src/utils/evaluateCondition.ts
@@ -3,16 +3,20 @@ import type { ConditionObject, EvaluateOptions } from "../types";
 import { EndpointError } from "../types";
 import { callFunction } from "./callFunction";
 
-export const evaluateCondition = ({ assign, ...fnArgs }: ConditionObject, options: EvaluateOptions) => {
+export const evaluateCondition = (condition: ConditionObject, options: EvaluateOptions) => {
+  const { assign } = condition;
+
   if (assign && assign in options.referenceRecord) {
     throw new EndpointError(`'${assign}' is already defined in Reference Record.`);
   }
-  const value = callFunction(fnArgs, options);
+  const value = callFunction(condition, options);
 
-  options.logger?.debug?.(`${debugId} evaluateCondition: ${toDebugString(fnArgs)} = ${toDebugString(value)}`);
+  options.logger?.debug?.(`${debugId} evaluateCondition: ${toDebugString(condition)} = ${toDebugString(value)}`);
 
-  return {
-    result: value === "" ? true : !!value,
-    ...(assign != null && { toAssign: { name: assign, value } }),
-  };
+  const result = value === "" ? true : !!value;
+
+  if (assign != null) {
+    return { result, toAssign: { name: assign, value } };
+  }
+  return { result };
 };
