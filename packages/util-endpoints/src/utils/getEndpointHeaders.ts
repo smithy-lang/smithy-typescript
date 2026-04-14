6a00f07cd2f9677a4e36c8a6fbbfcf712897589e
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2026-04-14 15:41:40 -0700

    perf(util-endpoints): mutate accumulator in getEndpointHeaders reduce

--- packages/util-endpoints/src/utils/getEndpointHeaders.ts
+++ packages/util-endpoints/src/utils/getEndpointHeaders.ts
@@ -3,16 +3,13 @@ import { EndpointError } from "../types";
 import { evaluateExpression } from "./evaluateExpression";
 
 export const getEndpointHeaders = (headers: EndpointObjectHeaders, options: EvaluateOptions) =>
-  Object.entries(headers ?? {}).reduce(
-    (acc, [headerKey, headerVal]) => ({
-      ...acc,
-      [headerKey]: headerVal.map((headerValEntry) => {
-        const processedExpr = evaluateExpression(headerValEntry, "Header value entry", options);
-        if (typeof processedExpr !== "string") {
-          throw new EndpointError(`Header '${headerKey}' value '${processedExpr}' is not a string`);
-        }
-        return processedExpr;
-      }),
-    }),
-    {}
-  );
+  Object.entries(headers ?? {}).reduce((acc, [headerKey, headerVal]) => {
+    acc[headerKey] = headerVal.map((headerValEntry) => {
+      const processedExpr = evaluateExpression(headerValEntry, "Header value entry", options);
+      if (typeof processedExpr !== "string") {
+        throw new EndpointError(`Header '${headerKey}' value '${processedExpr}' is not a string`);
+      }
+      return processedExpr;
+    });
+    return acc;
+  }, {});
