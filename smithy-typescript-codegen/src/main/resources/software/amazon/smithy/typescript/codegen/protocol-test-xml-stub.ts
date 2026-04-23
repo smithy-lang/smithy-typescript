b3bd930047851ae4765a0a45f3ca4c10dbd9b481
George Fu <kuhe@users.noreply.github.com>
2026-04-23 11:20:21 -0400

    remove custom entity code in xml protocol test stub

--- smithy-typescript-codegen/src/main/resources/software/amazon/smithy/typescript/codegen/protocol-test-xml-stub.ts
+++ smithy-typescript-codegen/src/main/resources/software/amazon/smithy/typescript/codegen/protocol-test-xml-stub.ts
@@ -2,24 +2,24 @@
  * Returns a map of key names that were un-equal to value objects showing the
  * discrepancies between the components.
  */
-const compareEquivalentXmlBodies = (
-  expectedBody: string,
-  generatedBody: string
-): Object => {
+const compareEquivalentXmlBodies = (expectedBody: string, generatedBody: string): Object => {
   const parseConfig = {
     attributeNamePrefix: "",
+    processEntities: {
+      enabled: true,
+      maxTotalExpansions: Infinity,
+    },
     htmlEntities: true,
     ignoreAttributes: false,
     ignoreDeclaration: true,
     parseTagValue: false,
     trimValues: false,
     tagValueProcessor: (_: any, val: any) => (val.trim() === "" && val.includes("\n") ? "" : undefined),
+    maxNestedTags: Infinity,
   };
 
   const parseXmlBody = (body: string) => {
     const parser = new XMLParser(parseConfig);
-    parser.addEntity("#xD", "\r");
-    parser.addEntity("#10", "\n");
     const parsedObj = parser.parse(body);
     const textNodeName = "#text";
     const key = Object.keys(parsedObj)[0];
