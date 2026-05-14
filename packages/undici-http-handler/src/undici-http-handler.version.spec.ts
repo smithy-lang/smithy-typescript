abdb6612bdfae8c50775267749fc66113f26c3fc
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2026-05-14 22:09:45 +0000

    test: use import in '.version.spec.ts'

--- packages/undici-http-handler/src/undici-http-handler.version.spec.ts
+++ packages/undici-http-handler/src/undici-http-handler.version.spec.ts
@@ -1,8 +1,6 @@
-import { createRequire } from "node:module";
 import { describe, expect, it } from "vitest";
 
-const require = createRequire(import.meta.url);
-const { dependencies, peerDependencies } = require("../package.json");
+import { dependencies, peerDependencies } from "../package.json";
 
 describe("undici version", () => {
   it("dependencies[undici] should match peerDependencies[undici]", () => {
