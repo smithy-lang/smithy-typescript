5234a2c9326f530acf7179323cc30bae2635ec4a
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2026-05-14 22:38:02 +0000

    test: suggest update undici version when dropping support for node major version

--- packages/undici-http-handler/src/undici-http-handler.version.spec.ts
+++ packages/undici-http-handler/src/undici-http-handler.version.spec.ts
@@ -1,9 +1,19 @@
 import { describe, expect, it } from "vitest";
 
-import { dependencies, peerDependencies } from "../package.json";
+import { dependencies, engines, peerDependencies } from "../package.json";
+
+const EXPECTED_ENGINES_NODE = ">=18.0.0";
 
 describe("undici version", () => {
   it("dependencies[undici] should match peerDependencies[undici]", () => {
     expect(dependencies["undici"]).toEqual(peerDependencies["undici"]);
+  });
+
+  it(`engines.node should be ${EXPECTED_ENGINES_NODE}`, () => {
+    expect(
+      engines.node,
+      "Note: If engines.node was updated to drop support for Node.js major version," +
+        " check if undici version can also be updated with this change."
+    ).toEqual(EXPECTED_ENGINES_NODE);
   });
 });
