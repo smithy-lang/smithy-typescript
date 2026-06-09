66c1ff999f97f69f72598040dd0b10c4d14a633d
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2026-06-09 15:46:34 -0700

    test(undici-http-handler): remove hardcoded EXPECTED_ENGINES_NODE (#2081)

--- packages/undici-http-handler/src/undici-http-handler.version.spec.ts
+++ packages/undici-http-handler/src/undici-http-handler.version.spec.ts
@@ -3,26 +3,17 @@ import { describe, expect, it } from "vitest";
 
 import { dependencies, engines, peerDependencies } from "../package.json";
 
-const EXPECTED_ENGINES_NODE = ">=20.18.1";
-
 describe("undici version", () => {
   it("peerDependencies[undici] should accept dependencies[undici]", () => {
     expect(peerDependencies["undici"]).toEqual(`>=${dependencies["undici"].replace("^", "")}`);
-  });
-
-  it(`engines.node should be ${EXPECTED_ENGINES_NODE}`, () => {
-    expect(
-      engines.node,
-      "Note: If engines.node was updated to drop support for Node.js major version," +
-        " the undici version should ideally be also updated with this change."
-    ).toEqual(EXPECTED_ENGINES_NODE);
   });
 
   it("engines.node should match undici's engines.node", () => {
     expect(
       engines.node,
       "Note: undici-http-handler engines.node should be the same as undici's engines.node." +
-        " If undici updated it's engines requirement, update undici-http-handler to match."
+        " If undici updated its engines requirement as part of a new major version," +
+        " update undici-http-handler engines.node to match and publish a new major version."
     ).toEqual(undiciEngines.node);
   });
 });
